a02969c492102275140f1472f769d0ef107f03af
Steven McCoy <fnjordy@gmail.com>
2007-05-29 14:53:53 +0000

    * Added FEC variables to transport object, no code yet. * Update Reed Solomon test to 8 bit symbols and popular CCSDS (255,223) parameters.

--- openpgm/pgm/test_rs.c
+++ openpgm/pgm/test_rs.c
@@ -24,14 +24,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <netinet/ip.h>
 
 #include <glib.h>
 
 #include "reed_solomon.h"
 #include "timer.h"
+#include "pgm.h"
 
 
 /* globals */
+
+static int g_max_tpdu = 1500;
+
+
 
 static void
 usage (const char* bin)
@@ -65,16 +71,23 @@ main (
 
 	time_init();
 
-/* data block */
-	guint8 data8[512];
+/* data block for CCSDS(255,223) */
+	guint8 data8[223];
 	for (int i = 0; i < G_N_ELEMENTS(data8); i++)
 		data8[i] = i % 255;
 
-/* FEC engine */
-	struct rs_control *rs = init_rs (10, 0x409, 0, 1, 6);
+/* FEC engine:
+ *
+ * symbol size:			8 bits
+ * primitive polynomial:	x^8 + x^4 + x^3 + x^2 + 1
+ * first consecutive root:	?
+ * primitive element for roots:	?
+ * number of roots:		32 (2t roots)
+ */
+	struct rs_control *rs = init_rs (8, 0x171, 0, 1, 32);
 
-/* parity block */
-	guint16 parity[6];
+/* parity block: must be set to 0 */
+	guint16 parity[32];
 	memset (parity, 0, sizeof(parity));
 
 	guint64 start, end, elapsed;
@@ -83,7 +96,7 @@ main (
 /* test encoding */
 	start = time_update_now();
 
-	encode_rs8 (rs, data8, 512, parity, 0);
+	encode_rs8 (rs, data8, sizeof(data8), parity, 0);
 
 	end = time_update_now();
 	elapsed = end - start;
@@ -102,7 +115,7 @@ main (
 
 	start = time_update_now();
 
-	int numerr = decode_rs8 (rs, data8, parity, 512, NULL, 0, NULL, 0, NULL);
+	int numerr = decode_rs8 (rs, data8, parity, sizeof(data8), NULL, 0, NULL, 0, NULL);
 
 	end = time_update_now();
 	elapsed = end - start;
