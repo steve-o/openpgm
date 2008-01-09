3caacee2bdbf0ad56c5b2aa126eddff3db9cb264
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2008-01-09 09:40:41 +0000

    * New test: NAK completion by ODATA. * New test: NAK completion by RDATA. * Rename suppression test as their is only one type.

--- trunk/openpgm/pgm/test/odata_completion.pl
+++ trunk/openpgm/pgm/test/odata_completion.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# suppression_by_ncf.pl
+# odata_completion.pl
 # 6.3. Data Recovery by Negative Acknowledgment
 
 use strict;
@@ -61,35 +61,21 @@ print "sim: waiting 2 seconds for erroneous NAKs ...\n";
 $sim->die_on_nak ({ 'timeout' => 2 });
 print "sim: no NAKs received.\n";
 
-## first run through with regular NAK generation to get regular backoff interval
 print "sim: publish ODATA sqn 90,003.\n";
 $sim->say ("net send odata ao 90003 90001 ichigo");
-my $t0 = [gettimeofday];
 print "sim: waiting for valid NAK.\n";
 $sim->wait_for_nak;
-my $normal_backoff = tv_interval ( $t0, [gettimeofday] );
-print "sim: NAK received in $normal_backoff seconds.\n";
+print "sim: NAK received.\n";
 
-## cleanup by publishing repair data
-print "sim: publish RDATA sqn 90,002.\n";
+print "sim: publish ODATA sqn 90,002.\n";
 $sim->say ("net send odata ao 90002 90001 momo");
-print "app: wait for data ...\n";
-my $data = $app->wait_for_data;
-print "app: data received [$data].\n";
 
-## second run with NAK suppression
-$t0 = [gettimeofday];
-print "sim: publish ODATA sqn 90,005.\n";
-$sim->say ("net send odata ao 90005 90001 anzu");
-print "sim: publish NCF sqn 90,004.\n";
-$sim->say ("net send ncf ao $odata->{PGM}->{gsi}.$odata->{PGM}->{sourcePort} 90004");
-
-print "sim: waiting for valid NAK.\n";
-$sim->wait_for_nak;
-my $suppressed_backoff = tv_interval ( $t0, [gettimeofday] );
-print "sim: NAK received in $suppressed_backoff seconds.\n";
-
-die "NAK suppression failed.\n" unless ($suppressed_backoff > $normal_backoff);
+for (1..2)
+{
+	print "app: wait for data ...\n";
+	my $data = $app->wait_for_data;
+	print "app: data received [$data].\n";
+}
 
 print "test completed successfully.\n";
 
