3210c9988cd3badfa690c852aab1e88602d1cee8
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2008-02-19 15:54:46 +0000

    * Add LGPL/GPL COPYING notices. * GPL code now builds to libpgmplus, LGPL to libpgm.

--- trunk/openpgm/pgm/csum-partial64.c
+++ trunk/openpgm/pgm/csum-partial64.c
@@ -5,7 +5,7 @@
  * Copyright 2002 Andi Kleen, SuSE Labs.
  *
  * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file COPYING in the main directory of this archive
+ * License.  See the file COPYING.GPL in the main directory of this archive
  * for more details. No warranty for anything given at all.
  */
 
