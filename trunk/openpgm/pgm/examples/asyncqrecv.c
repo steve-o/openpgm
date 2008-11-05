73eae1d9f2954f1b17a3d8af3f9b5e7372258a64
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2008-11-05 04:52:07 +0000

    Update examples with less header files.

--- trunk/openpgm/pgm/examples/asyncqrecv.c
+++ trunk/openpgm/pgm/examples/asyncqrecv.c
@@ -37,13 +37,9 @@
 
 #include <glib.h>
 
-#include <pgm/if.h>
+#include <pgm/pgm.h>
 #include <pgm/backtrace.h>
 #include <pgm/log.h>
-#include <pgm/transport.h>
-#include <pgm/gsi.h>
-#include <pgm/signal.h>
-#include <pgm/async.h>
 
 
 /* typedefs */
