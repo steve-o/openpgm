a53e8ab3ad7a429485a5c7229a407c0ee4d1898b
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2008-11-15 16:23:11 +0000

    tagging 1.0.0 release candidate 3

---
+++ tags/release-1-0-0rc3/openpgm/pgm/dev/block_send.c
@@ -0,0 +1,260 @@
+/* vim:ts=8:sts=8:sw=4:noai:noexpandtab
+ *
+ * Large APDU sender using the PGM transport, similar to pgmsend.
+ *
+ * Copyright (c) 2006-2007 Miru Limited.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+#include <errno.h>
+#include <getopt.h>
+#include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+#include <glib.h>
+
+#include "pgm/if.h"
+#include "pgm/backtrace.h"
+#include "pgm/log.h"
+#include "pgm/transport.h"
+#include "pgm/gsi.h"
+#include "pgm/signal.h"
+
+
+/* typedefs */
+
+/* globals */
+
+static int g_port = 7500;
+static const char* g_network = ";226.0.0.1";
+
+static int g_odata_interval = 1 * 1000;
+static int g_payload = 0;
+static int g_max_tpdu = 1500;
+static int g_sqns = 10;
+
+static pgm_transport_t* g_transport = NULL;
+
+static GMainLoop* g_loop = NULL;
+
+
+static void on_signal (int);
+static gboolean on_startup (gpointer);
+static gboolean on_mark (gpointer);
+
+static void send_odata (void);
+static gboolean on_odata_timer (gpointer);
+
+
+G_GNUC_NORETURN static void
+usage (const char* bin)
+{
+	fprintf (stderr, "Usage: %s [options]\n", bin);
+	fprintf (stderr, "  -n <network>    : Multicast group or unicast IP address\n");
+	fprintf (stderr, "  -s <port>       : IP port\n");
+	exit (1);
+}
+
+int
+main (
+	int	argc,
+	char   *argv[]
+	)
+{
+	g_message ("block_send");
+
+/* parse program arguments */
+	const char* binary_name = strrchr (argv[0], '/');
+	int c;
+	while ((c = getopt (argc, argv, "s:n:c:h")) != -1)
+	{
+		switch (c) {
+		case 'n':	g_network = optarg; break;
+		case 's':	g_port = atoi (optarg); break;
+
+		case 'h':
+		case '?': usage (binary_name);
+		}
+	}
+
+	log_init ();
+	pgm_init ();
+
+	g_loop = g_main_loop_new(NULL, FALSE);
+
+/* setup signal handlers */
+	pgm_signal_install (SIGSEGV, on_sigsegv);
+	pgm_signal_install (SIGINT, on_signal);
+	pgm_signal_install (SIGTERM, on_signal);
+	pgm_signal_install (SIGHUP, SIG_IGN);
+
+/* delayed startup */
+	g_message ("scheduling startup.");
+	g_timeout_add (0, (GSourceFunc)on_startup, NULL);
+
+/* dispatch loop */
+	g_message ("entering main event loop ... ");
+	g_main_loop_run (g_loop);
+
+	g_message ("event loop terminated, cleaning up.");
+
+/* cleanup */
+	g_main_loop_unref (g_loop);
+	g_loop = NULL;
+
+	if (g_transport) {
+		g_message ("destroying transport.");
+
+		pgm_transport_destroy (g_transport, TRUE);
+		g_transport = NULL;
+	}
+
+	g_message ("finished.");
+	return 0;
+}
+
+static void
+on_signal (
+	G_GNUC_UNUSED int	signum
+	)
+{
+	g_message ("on_signal");
+
+	g_main_loop_quit(g_loop);
+}
+
+static gboolean
+on_startup (
+	G_GNUC_UNUSED gpointer data
+	)
+{
+	g_message ("startup.");
+	g_message ("create transport.");
+
+	pgm_gsi_t gsi;
+#if 0
+	char hostname[NI_MAXHOST];
+	struct addrinfo hints, *res = NULL;
+
+	gethostname (hostname, sizeof(hostname));
+	memset (&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_flags = AI_ADDRCONFIG;
+	getaddrinfo (hostname, NULL, &hints, &res);
+	int e = pgm (((struct sockaddr_in*)(res->ai_addr))->sin_addr, &gsi);
+	g_assert (e == 0);
+	freeaddrinfo (res);
+#else
+	int e = pgm_create_md5_gsi (&gsi);
+	g_assert (e == 0);
+#endif
+
+	struct group_source_req recv_gsr, send_gsr;
+	int gsr_len = 1;
+	e = pgm_if_parse_transport (g_network, AF_INET, &recv_gsr, &send_gsr, &gsr_len);
+	g_assert (e == 0);
+	g_assert (gsr_len == 1);
+
+	e = pgm_transport_create (&g_transport, &gsi, g_port, &recv_gsr, 1, &send_gsr);
+	g_assert (e == 0);
+
+	pgm_transport_set_max_tpdu (g_transport, g_max_tpdu);
+	pgm_transport_set_txw_sqns (g_transport, g_sqns);
+	pgm_transport_set_rxw_sqns (g_transport, g_sqns);
+	pgm_transport_set_hops (g_transport, 16);
+	pgm_transport_set_ambient_spm (g_transport, 8192*1000);
+	guint spm_heartbeat[] = { 1*1000, 1*1000, 2*1000, 4*1000, 8*1000, 16*1000, 32*1000, 64*1000, 128*1000, 256*1000, 512*1000, 1024*1000, 2048*1000, 4096*1000, 8192*1000 };
+	pgm_transport_set_heartbeat_spm (g_transport, spm_heartbeat, G_N_ELEMENTS(spm_heartbeat));
+	pgm_transport_set_peer_expiry (g_transport, 5*8192*1000);
+	pgm_transport_set_spmr_expiry (g_transport, 250*1000);
+
+	e = pgm_transport_bind (g_transport);
+	if (e != 0) {
+		g_critical ("pgm_transport_bind failed errno %i: \"%s\"", e, strerror(e));
+		G_BREAKPOINT();
+	}
+
+/* period timer to indicate some form of life */
+// TODO: Gnome 2.14: replace with g_timeout_add_seconds()
+	g_timeout_add(10 * 1000, (GSourceFunc)on_mark, NULL);
+
+	g_message ("scheduling ODATA broadcasts every %.1f secs.", g_odata_interval / 1000.0);
+	g_timeout_add(g_odata_interval, (GSourceFunc)on_odata_timer, NULL);
+
+	g_message ("startup complete.");
+	return FALSE;
+}
+
+/* we send out a stream of ODATA packets with basic changing payload
+ */
+
+static gboolean
+on_odata_timer (
+	G_GNUC_UNUSED gpointer data
+	)
+{
+	send_odata ();
+	return TRUE;
+}
+
+static void
+send_odata (void)
+{
+	int e;
+	char payload_string[2000];
+	char number[100];
+
+	snprintf (number, sizeof(number), "%i ", g_payload++);
+
+	payload_string[0] = 0;
+	int count = 2000 / strlen (number);
+	for (int i = 0; i < count; i++)
+	{
+		strcat (payload_string, number);
+	}
+
+	e = pgm_transport_send (g_transport, payload_string, strlen(payload_string) + 1, 0);
+        if (e < 0) {
+		g_warning ("send failed.");
+                return;
+        }
+}
+
+/* idle log notification
+ */
+
+static gboolean
+on_mark (
+	G_GNUC_UNUSED gpointer data
+	)
+{
+	g_message ("counter: %i", g_payload);
+
+	return TRUE;
+}
+
+/* eof */
---
+++ tags/release-1-0-0rc3/openpgm/pgm/dev/test_cpu_timers.c
@@ -0,0 +1,138 @@
+/* vim:ts=8:sts=8:sw=4:noai:noexpandtab
+ *
+ * Test CPU/thread drift.
+ *
+ * Copyright (c) 2006-2007 Miru Limited.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+#define _GNU_SOURCE
+
+
+#include <sched.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <glib.h>
+
+
+
+/* globals */
+
+/* read time stamp counter, count of ticks from processor reset.
+ */
+
+static __inline__ uint64_t
+rdtsc (void)
+{
+	uint32_t lo, hi;
+
+/* We cannot use "=A", since this would use %rax on x86_64 */
+	__asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+
+	return (uint64_t)hi << 32 | lo;
+}
+
+
+G_GNUC_NORETURN static void
+usage (const char* bin)
+{
+	fprintf (stderr, "Usage: %s [options]\n", bin);
+	exit (1);
+}
+
+int
+main (
+	int	argc,
+	char   *argv[]
+	)
+{
+	puts ("test_cpu_timers");
+
+/* parse program arguments */
+	const char* binary_name = strrchr (argv[0], '/');
+	int c;
+	while ((c = getopt (argc, argv, "h")) != -1)
+	{
+		switch (c) {
+
+		case 'h':
+		case '?': usage (binary_name);
+		}
+	}
+
+/* setup signal handlers */
+	signal(SIGHUP, SIG_IGN);
+
+/* setup process affinity and maximum priority */
+	struct sched_param sp;
+	sp.sched_priority = sched_get_priority_max (SCHED_RR);
+	int e = sched_setscheduler (0, SCHED_RR, &sp);
+	g_assert (!e);
+
+	cpu_set_t cpu0, cpu1, cur;
+
+	e = sched_getaffinity (0, sizeof(cpu_set_t), &cur);
+	g_assert (!e);
+
+	CPU_ZERO(&cpu0);
+	CPU_ZERO(&cpu1);
+	CPU_SET(0, &cpu0);
+	CPU_SET(1, &cpu1);
+
+	uint64_t h0 = 0, h1 = 0, diff = 0;
+
+	for (int i = 0; i < 100000; i++)
+	{
+		e = sched_setaffinity (0, sizeof(cpu_set_t), &cpu0);
+		g_assert (!e);
+
+		uint64_t x = rdtsc();
+
+		e = sched_setaffinity (0, sizeof(cpu_set_t), &cpu1);
+		g_assert (!e);
+
+		uint64_t y = rdtsc();
+
+		e = sched_setaffinity (0, sizeof(cpu_set_t), &cpu0);
+		g_assert (!e);
+
+		uint64_t z = rdtsc();
+
+		if (!i || z - x < diff)
+		{
+			diff = z - x;
+			h0   = x + diff / 2;
+			h1   = y;
+		}
+	}
+
+	printf ("drift %lu ticks.\n", h0 - h1);
+
+	e = sched_setaffinity (0, sizeof(cpu_set_t), &cur);
+	g_assert (!e);
+
+
+	puts ("finished.");
+	return 0;
+}
+
+/* eof */
