219d42d9fdeb7401428ec870cc4710ed4dea203d
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2009-02-09 10:45:45 +0000

    * Rewrite of network parameter parsing in order to capture all error cases. * Bump to API version to match int -> gsize change on parameter. * Addition of 10 failure cases for network parameter parsing.

--- trunk/openpgm/pgm/check/check_if.c
+++ trunk/openpgm/pgm/check/check_if.c
@@ -36,18 +36,21 @@ static int	g_ai_family = 0;
 static void
 setup_unspec (void)
 {
+//	puts ("setup unspecified address family.");
 	g_ai_family = AF_UNSPEC;
 }
 
 static void
 setup_ipv4 (void)
 {
+//	puts ("setup IPv4 forced address family.");
 	g_ai_family = AF_INET;
 }
 
 static void
 setup_ipv6 (void)
 {
+//	puts ("setup IPv6 forced address family.");
 	g_ai_family = AF_INET6;
 }
 
@@ -105,6 +108,8 @@ match_any_source (
 
 	switch (ai_family) {
 	case AF_UNSPEC:
+		return ai_family == pgm_sockaddr_family(&gsr->gsr_source);
+
 	case AF_INET:
 		return 0 == pgm_sockaddr_cmp ((struct sockaddr*)&gsr->gsr_source, (const struct sockaddr*)&if_any_source);
 
@@ -213,10 +218,9 @@ START_TEST (test_parse_transport_000)
 	const char*		s		= (g_ai_family == AF_INET6) ? cases_000[_i].ipv6 : cases_000[_i].ipv4;
 	int			ai_family	= g_ai_family;
 	struct group_source_req	recv_gsr;
-	int			recv_len	= 1;
+	gsize			recv_len	= 1;
 	struct group_source_req send_gsr;
 
-	g_message ("%i: test_parse_transport_000(\"%s\")", _i, s);
 	fail_unless (0 == pgm_if_parse_transport (s, ai_family, &recv_gsr, &recv_len, &send_gsr));
 	fail_unless (1 == recv_len);
 
@@ -279,7 +283,7 @@ START_TEST (test_parse_transport_001)
 	const char*		s		= (g_ai_family == AF_INET6) ? cases_001[_i].ipv6 : cases_001[_i].ipv4;
 	int			ai_family	= g_ai_family;
 	struct group_source_req	recv_gsr[1];
-	int			recv_len	= G_N_ELEMENTS(recv_gsr);
+	gsize			recv_len	= G_N_ELEMENTS(recv_gsr);
 	struct group_source_req send_gsr[1];
 
 	g_message ("%i: test_parse_transport_001(%s, \"%s\")", _i, (ai_family == AF_INET6) ? "AF_INET6" : ( (ai_family == AF_INET) ? "AF_INET" : "AF_UNSPEC" ), s);
@@ -339,7 +343,7 @@ START_TEST (test_parse_transport_002)
 	const char*		s		= (g_ai_family == AF_INET6) ? cases_002[_i].ipv6 : cases_002[_i].ipv4;
 	int			ai_family	= g_ai_family;
 	struct group_source_req	recv_gsr[1];
-	int			recv_len	= G_N_ELEMENTS(recv_gsr);
+	gsize			recv_len	= G_N_ELEMENTS(recv_gsr);
 	struct group_source_req send_gsr[1];
 
 	fail_unless (0 == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, send_gsr));
@@ -363,7 +367,7 @@ START_TEST (test_parse_transport_003)
 							       /* AF_INET */: ";239.192.56.1;239.192.56.2";
 	int			ai_family	= g_ai_family;
 	struct group_source_req	recv_gsr[1];
-	int			recv_len	= 1;
+	gsize			recv_len	= 1;
 	struct group_source_req send_gsr[1];
 
 	fail_unless (0 == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, send_gsr));
@@ -377,7 +381,7 @@ START_TEST (test_parse_transport_003)
 		fail_unless (0 == pgm_sockaddr_cmp ((struct sockaddr*)&recv_gsr[0].gsr_group, (struct sockaddr*)&addr));
 
 		inet_pton (AF_INET6, "ff08::2", &((struct sockaddr_in6*)&addr)->sin6_addr);
-		((struct sockaddr_in*)&addr)->sin_family = AF_INET;
+		((struct sockaddr_in*)&addr)->sin_family = AF_INET6;
 		fail_unless (0 == pgm_sockaddr_cmp ((struct sockaddr*)&send_gsr[0].gsr_group, (struct sockaddr*)&addr));
 	}
 	else
@@ -407,7 +411,7 @@ START_TEST (test_parse_transport_004)
 							       /* AF_INET */: ";239.192.56.1,239.192.56.2;239.192.56.3";
 	int			ai_family	= g_ai_family;
 	struct group_source_req	recv_gsr[2];
-	int			recv_len	= 2;
+	gsize			recv_len	= 2;
 	struct group_source_req send_gsr[1];
 
 	fail_unless (0 == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, send_gsr));
@@ -417,14 +421,14 @@ START_TEST (test_parse_transport_004)
 	{
 		struct sockaddr_storage addr;
 		inet_pton (AF_INET6, "ff08::1", &((struct sockaddr_in6*)&addr)->sin6_addr);
-		((struct sockaddr_in*)&addr)->sin_family = AF_INET;
+		((struct sockaddr_in*)&addr)->sin_family = AF_INET6;
 		fail_unless (0 == pgm_sockaddr_cmp ((struct sockaddr*)&recv_gsr[0].gsr_group, (struct sockaddr*)&addr));
 		inet_pton (AF_INET6, "ff08::2", &((struct sockaddr_in6*)&addr)->sin6_addr);
-		((struct sockaddr_in*)&addr)->sin_family = AF_INET;
+		((struct sockaddr_in*)&addr)->sin_family = AF_INET6;
 		fail_unless (0 == pgm_sockaddr_cmp ((struct sockaddr*)&recv_gsr[1].gsr_group, (struct sockaddr*)&addr));
 
 		inet_pton (AF_INET6, "ff08::3", &((struct sockaddr_in6*)&addr)->sin6_addr);
-		((struct sockaddr_in*)&addr)->sin_family = AF_INET;
+		((struct sockaddr_in*)&addr)->sin_family = AF_INET6;
 		fail_unless (0 == pgm_sockaddr_cmp ((struct sockaddr*)&send_gsr[0].gsr_group, (struct sockaddr*)&addr));
 	}
 	else
@@ -447,49 +451,157 @@ START_TEST (test_parse_transport_004)
 END_TEST
 
 
-/* too many semi-colons
+/* too many interfaces
  */
 START_TEST (test_parse_transport_fail_000)
 {
-	const char*		s		= ";;;";
+	const char*		s		= "eth0,lo;;;";
 	int			ai_family	= AF_UNSPEC;
 	struct group_source_req	recv_gsr[1];
-	int			recv_len	= G_N_ELEMENTS(recv_gsr);
+	gsize			recv_len	= G_N_ELEMENTS(recv_gsr);
 	struct group_source_req send_gsr[1];
 
 	fail_unless (-EINVAL == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, send_gsr));
 }
 END_TEST
 
-/* too many interfaces
- */
-
 /* invalid characters, or simply just bogus
  */
+START_TEST (test_parse_transport_fail_001)
+{
+        const char*             s               = "!@#$%^&*()";
+        int                     ai_family       = AF_UNSPEC;
+        struct group_source_req recv_gsr[1];
+        gsize                     recv_len        = G_N_ELEMENTS(recv_gsr);
+        struct group_source_req send_gsr[1];
+
+        fail_unless (-EINVAL == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, send_gsr));
+}
+END_TEST
 
 /* too many groups
  */
+START_TEST (test_parse_transport_fail_002)
+{
+        const char*             s               = ";239.192.0.1,239.192.0.2,239.192.0.3,239.192.0.4,239.192.0.5,239.192.0.6,239.192.0.7,239.192.0.8,239.192.0.9,239.192.0.10,239.192.0.11,239.192.0.12,239.192.0.13,239.192.0.14,239.192.0.15,239.192.0.16,239.192.0.17,239.192.0.18,239.192.0.19,239.192.0.20;239.192.0.21";
+        int                     ai_family       = AF_UNSPEC;
+        struct group_source_req recv_gsr[1];
+        gsize                     recv_len        = G_N_ELEMENTS(recv_gsr);
+        struct group_source_req send_gsr[1];
+
+        fail_unless (-EINVAL == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, send_gsr));
+}
+END_TEST
 
 /* too many receiver groups in asymmetric pairing
  */
+START_TEST (test_parse_transport_fail_003)
+{
+        const char*             s               = ";239.192.0.1,239.192.0.2,239.192.0.3,239.192.0.4,239.192.0.5,239.192.0.6,239.192.0.7,239.192.0.8,239.192.0.9,239.192.0.10,239.192.0.11,239.192.0.12,239.192.0.13,239.192.0.14,239.192.0.15,239.192.0.16,239.192.0.17,239.192.0.18,239.192.0.19,239.192.0.20,239.192.0.21;239.192.0.22";
+        int                     ai_family       = AF_UNSPEC;
+        struct group_source_req recv_gsr[1];
+        gsize                     recv_len        = G_N_ELEMENTS(recv_gsr);
+        struct group_source_req send_gsr[1];
+
+        fail_unless (-EINVAL == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, send_gsr));
+}
+END_TEST
 
 /* null string
  */
+START_TEST (test_parse_transport_fail_004)
+{
+        const char*             s               = NULL;
+        int                     ai_family       = AF_UNSPEC;
+        struct group_source_req recv_gsr[1];
+        gsize                     recv_len        = G_N_ELEMENTS(recv_gsr);
+        struct group_source_req send_gsr[1];
+
+        fail_unless (-EINVAL == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, send_gsr));
+}
+END_TEST
 
 /* invalid address family
  */
+START_TEST (test_parse_transport_fail_005)
+{
+        const char*             s               = ";";
+        int                     ai_family       = AF_IPX;
+        struct group_source_req recv_gsr[1];
+        gsize                     recv_len        = G_N_ELEMENTS(recv_gsr);
+        struct group_source_req send_gsr[1];
+
+        fail_unless (-EINVAL == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, send_gsr));
+}
+END_TEST
 
 /* invalid receive group pointer
  */
+START_TEST (test_parse_transport_fail_006)
+{
+        const char*             s               = ";";
+        int                     ai_family       = AF_UNSPEC;
+        gsize                     recv_len        = 1;            /* bogus but valid */
+        struct group_source_req send_gsr[1];
+
+        fail_unless (-EINVAL == pgm_if_parse_transport (s, ai_family, NULL, &recv_len, send_gsr));
+}
+END_TEST
 
 /* zero length receive group size
  */
+START_TEST (test_parse_transport_fail_007)
+{
+        const char*             s               = ";";
+        int                     ai_family       = AF_UNSPEC;
+        struct group_source_req recv_gsr[1];
+        gsize                     recv_len        = 0;
+        struct group_source_req send_gsr[1];
+
+        fail_unless (-EINVAL == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, send_gsr));
+}
+END_TEST
+
+/* invalid length receive group size
+ */
+START_TEST (test_parse_transport_fail_008)
+{
+        const char*             s               = ";";
+        int                     ai_family       = AF_UNSPEC;
+        struct group_source_req recv_gsr[1];
+        struct group_source_req send_gsr[1];
+
+        fail_unless (-EINVAL == pgm_if_parse_transport (s, ai_family, recv_gsr, NULL, send_gsr));
+}
+END_TEST
 
 /* invalid send group pointer
  */
+START_TEST (test_parse_transport_fail_009)
+{
+        const char*             s               = ";";
+        int                     ai_family       = AF_UNSPEC;
+        struct group_source_req recv_gsr[1];
+        gsize                     recv_len        = G_N_ELEMENTS(recv_gsr);
+
+        fail_unless (-EINVAL == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, NULL));
+}
+END_TEST
 
 /* more receive groups than supplied limit
  */
+START_TEST (test_parse_transport_fail_010)
+{
+        const char*             s               = ";239.192.0.1,239.192.0.2;239.192.0.3";
+        int                     ai_family       = AF_UNSPEC;
+        struct group_source_req recv_gsr[1];
+        gsize                     recv_len        = 1;
+        struct group_source_req send_gsr[1];
+
+        fail_unless (-ENOMEM == pgm_if_parse_transport (s, ai_family, recv_gsr, &recv_len, send_gsr));
+}
+END_TEST
+
 
 Suite*
 make_if_suite (void)
@@ -504,7 +616,6 @@ make_if_suite (void)
 	tcase_add_checked_fixture (tc_parse_transport_ipv4, setup_ipv4, teardown);
 	tcase_add_checked_fixture (tc_parse_transport_ipv6, setup_ipv6, teardown);
 
-#if 0
 /* unspecified address family, ai_family == AF_UNSPEC */
 	suite_add_tcase (s, tc_parse_transport_unspec);
 	tcase_add_loop_test (tc_parse_transport_unspec, test_parse_transport_000, 0, G_N_ELEMENTS(cases_000));
@@ -512,7 +623,18 @@ make_if_suite (void)
 	tcase_add_loop_test (tc_parse_transport_unspec, test_parse_transport_002, 0, G_N_ELEMENTS(cases_002));
 	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_003);
 	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_004);
-//	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_000);
+
+	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_000);
+	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_001);
+	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_002);
+	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_003);
+	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_004);
+	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_005);
+	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_006);
+	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_007);
+	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_008);
+	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_009);
+	tcase_add_test (tc_parse_transport_unspec, test_parse_transport_fail_010);
 
 /* forced IPv4 */
 	suite_add_tcase (s, tc_parse_transport_ipv4);
@@ -521,17 +643,14 @@ make_if_suite (void)
 	tcase_add_loop_test (tc_parse_transport_ipv4, test_parse_transport_002, 0, G_N_ELEMENTS(cases_002));
 	tcase_add_test (tc_parse_transport_ipv4, test_parse_transport_003);
 	tcase_add_test (tc_parse_transport_ipv4, test_parse_transport_004);
-#endif
 
 /* forced IPv6 */
 	suite_add_tcase (s, tc_parse_transport_ipv6);
 	tcase_add_loop_test (tc_parse_transport_ipv6, test_parse_transport_000, 0, G_N_ELEMENTS(cases_000));
 	tcase_add_loop_test (tc_parse_transport_ipv6, test_parse_transport_001, 0, G_N_ELEMENTS(cases_001));
-/*
 	tcase_add_loop_test (tc_parse_transport_ipv6, test_parse_transport_002, 0, G_N_ELEMENTS(cases_002));
 	tcase_add_test (tc_parse_transport_ipv6, test_parse_transport_003);
 	tcase_add_test (tc_parse_transport_ipv6, test_parse_transport_004);
-*/
 
 	return s;
 }
