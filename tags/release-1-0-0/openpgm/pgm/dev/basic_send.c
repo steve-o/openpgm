c0fc03c7e7fcff3b0b94676927350c04e379ee20
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2009-05-15 03:54:57 +0000

    tagging 1.0.0 release

---
+++ tags/release-1-0-0/openpgm/pgm/dev/basic_send.c
@@ -0,0 +1,460 @@
+/* vim:ts=8:sts=8:sw=4:noai:noexpandtab
+ *
+ * Send very basic packets.
+ *
+ * Copyright (c) 2006-2008 Miru Limited.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <errno.h>
+#include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
+#include <glib.h>
+
+#include "pgm/backtrace.h"
+#include "pgm/log.h"
+#include "pgm/packet.h"
+#include "pgm/checksum.h"
+
+/* OpenSolaris */
+#ifndef MSG_CONFIRM
+#	define MSG_CONFIRM	0
+#endif
+
+/* globals */
+
+static int g_port = 7500;
+static const char* g_network = "226.0.0.1";
+static struct ip_mreq g_mreq;
+
+static const char* g_payload = "banana man!";
+
+/* MTU is messy, 1500 for regular ethernet, 9000 for jumbo but various jumbo
+ * sizes in between are only supported by some hardware, similarly some IP
+ * options can reduce the maxium size, e.g. LLC/SNAP or VPN encapsulation.
+ * 1492 is good for ATM.
+ */
+static int g_mtu = 1500;
+#ifdef __USE_BSD
+#	define g_tpdu_limit  (g_mtu - sizeof(struct ip))
+#else
+#	define g_tpdu_limit  (g_mtu - sizeof(struct iphdr))
+#endif
+
+/* TSDU limit is more complicated as PGM options reduce the available size
+ */
+#define g_tsdu_limit  (g_tpdu_limit - sizeof(struct pgm_header) - sizeof(struct pgm_data))
+
+static int g_io_channel_sock = -1;
+static GIOChannel* g_io_channel = NULL;
+static GMainLoop* g_loop = NULL;
+
+
+void banana_man (void);
+
+static void on_signal (int);
+static gboolean on_startup (gpointer);
+static gboolean on_mark (gpointer);
+
+
+G_GNUC_NORETURN static void
+usage (
+	const char*	bin
+	)
+{
+	fprintf (stderr, "Usage: %s [options] message...\n", bin);
+	fprintf (stderr, "  -n <network>    : Multicast group or unicast IP address\n");
+	fprintf (stderr, "  -s <port>       : IP port\n");
+	exit (1);
+}
+
+int
+main (
+	int		argc,
+	char*		argv[]
+	)
+{
+	puts ("basic_send");
+
+/* parse program arguments */
+	const char* binary_name = strrchr (argv[0], '/');
+	int c;
+	while ((c = getopt (argc, argv, "s:n:h")) != -1)
+	{
+		switch (c) {
+		case 'n':	g_network = optarg; break;
+		case 's':	g_port = atoi (optarg); break;
+
+		case 'h':
+		case '?': usage (binary_name);
+		}
+	}
+
+	if (optind < argc) {
+		g_payload = argv[optind];
+		printf ("payload \"%s\"\n", g_payload);
+	}
+
+	if (strlen(g_payload) > g_tsdu_limit) {
+		printf ("WARNING: payload string is longer than the interface MTU will allow.\n");
+	}
+
+	log_init ();
+
+/* setup signal handlers */
+	signal(SIGSEGV, on_sigsegv);
+	signal(SIGINT, on_signal);
+	signal(SIGTERM, on_signal);
+	signal(SIGHUP, SIG_IGN);
+
+/* delayed startup */
+	puts ("scheduling startup.");
+	g_timeout_add(0, (GSourceFunc)on_startup, NULL);
+
+/* dispatch loop */
+	g_loop = g_main_loop_new(NULL, FALSE);
+
+	puts ("entering main event loop ... ");
+	g_main_loop_run(g_loop);
+
+	puts ("event loop terminated, cleaning up.");
+
+/* cleanup */
+	g_main_loop_unref(g_loop);
+	g_loop = NULL;
+
+	if (g_io_channel) {
+		puts ("closing io channel.");
+
+		GError *err = NULL;
+		g_io_channel_shutdown (g_io_channel, FALSE, &err);
+		g_io_channel = NULL;
+	}
+
+	if (g_io_channel_sock) {
+		puts ("closing socket.");
+
+		close(g_io_channel_sock);
+		g_io_channel_sock = -1;
+	}
+
+	puts ("finished.");
+	return 0;
+}
+
+static void
+on_signal (
+	G_GNUC_UNUSED int signum
+	)
+{
+	puts ("on_signal");
+
+	g_main_loop_quit(g_loop);
+}
+
+static gboolean
+on_startup (
+	G_GNUC_UNUSED gpointer data
+	)
+{
+	int e;
+
+	puts ("startup.");
+
+/* find PGM protocol id */
+// TODO: fix valgrind errors
+	int ipproto_pgm = IPPROTO_PGM;
+#if HAVE_GETPROTOBYNAME_R
+	char b[1024];
+	struct protoent protobuf, *proto;
+	e = getprotobyname_r("pgm", &protobuf, b, sizeof(b), &proto);
+	if (e != -1 && proto != NULL) {
+		if (proto->p_proto != ipproto_pgm) {
+			printf("Setting PGM protocol number to %i from /etc/protocols.\n");
+			ipproto_pgm = proto->p_proto;
+		}
+	}
+#else
+	struct protoent *proto = getprotobyname("pgm");
+	if (proto != NULL) {
+		if (proto->p_proto != ipproto_pgm) {
+			printf("Setting PGM protocol number to %i from /etc/protocols.\n", proto->p_proto);
+			ipproto_pgm = proto->p_proto;
+		}
+	}
+#endif
+
+/* open socket for snooping */
+	puts ("opening raw socket.");
+	g_io_channel_sock = socket(PF_INET, SOCK_RAW, ipproto_pgm);
+	if (g_io_channel_sock < 0) {
+		int _e = errno;
+		perror("on_startup() failed");
+
+		if (_e == EPERM && 0 != getuid()) {
+			puts ("PGM protocol requires this program to run as superuser.");
+		}
+		g_main_loop_quit(g_loop);
+		return FALSE;
+	}
+
+/* drop out of setuid 0 */
+	if (0 == getuid()) {
+		puts ("dropping superuser privileges.");
+		setuid((gid_t)65534);
+		setgid((uid_t)65534);
+	}
+
+	char _t = 0;
+	e = setsockopt(g_io_channel_sock, IPPROTO_IP, IP_HDRINCL, &_t, sizeof(_t));
+	if (e < 0) {
+		perror("on_startup() failed");
+		close(g_io_channel_sock);
+		g_main_loop_quit(g_loop);
+		return FALSE;
+	}
+
+/* buffers */
+	int buffer_size = 0;
+	socklen_t len = 0;
+	e = getsockopt(g_io_channel_sock, SOL_SOCKET, SO_RCVBUF, &buffer_size, &len);
+	if (e == 0) {
+		printf ("receive buffer set at %i bytes.\n", buffer_size);
+	}
+	e = getsockopt(g_io_channel_sock, SOL_SOCKET, SO_SNDBUF, &buffer_size, &len);
+	if (e == 0) {
+		printf ("send buffer set at %i bytes.\n", buffer_size);
+	}
+
+/* bind */
+	struct sockaddr_in addr;
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	e = bind(g_io_channel_sock, (struct sockaddr*)&addr, sizeof(addr));
+	if (e < 0) {
+		perror("on_startup() failed");
+		close(g_io_channel_sock);
+		g_main_loop_quit(g_loop);
+		return FALSE;
+	}
+
+/* query bound socket for actual interface address */
+	char hostname[NI_MAXHOST + 1];
+	gethostname (hostname, sizeof(hostname));
+	struct hostent *he = gethostbyname (hostname);
+
+	if (he == NULL) {
+		printf ("error code %i\n", errno);
+		perror("on_startup() failed");
+		close(g_io_channel_sock);
+		g_main_loop_quit(g_loop);
+		return FALSE;
+	}
+
+	struct in_addr g_addr;
+	g_addr.s_addr = ((struct in_addr*)(he->h_addr_list[0]))->s_addr;
+	printf ("socket bound to %s (%s)\n", inet_ntoa(g_addr), hostname);
+
+/* check for MTU */
+	struct ifreq ifreq;
+	e = ioctl (g_io_channel_sock, SIOCGIFMTU, &ifreq);
+	if (e < 0) {
+		perror("Cannot determine interface MTU");
+	} else {
+#ifdef __USE_BSD
+		printf ("interface MTU %i bytes.\n", ifreq.ifr_metric);
+#else
+		printf ("interface MTU %i bytes.\n", ifreq.ifr_mtu);
+#endif
+	}
+
+/* multicast */
+	memset(&g_mreq, 0, sizeof(g_mreq));
+	g_mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+	printf ("sending on interface %s.\n", inet_ntoa(g_mreq.imr_interface));
+	g_mreq.imr_multiaddr.s_addr = inet_addr(g_network);
+	if (IN_MULTICAST(g_htonl(g_mreq.imr_multiaddr.s_addr)))
+		printf ("sending on multicast address %s.\n", inet_ntoa(g_mreq.imr_multiaddr));
+	else
+		printf ("sending unicast to address %s.\n", inet_ntoa(g_mreq.imr_multiaddr));
+
+	if (IN_MULTICAST(g_htonl(g_mreq.imr_multiaddr.s_addr)))
+	{
+/* IP_ADD_MEMBERSHIP = subscription
+ * IP_MULTICAST_IF = send only
+ */
+		e = setsockopt(g_io_channel_sock, IPPROTO_IP, IP_MULTICAST_IF, &g_mreq, sizeof(g_mreq));
+//		e = setsockopt(g_io_channel_sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &g_mreq, sizeof(g_mreq));
+		if (e < 0) {
+			perror("on_startup() failed");
+			close(g_io_channel_sock);
+			g_main_loop_quit(g_loop);
+			return FALSE;
+		}
+
+/* multicast loopback */
+		gboolean n = 0;
+		e = setsockopt(g_io_channel_sock, IPPROTO_IP, IP_MULTICAST_LOOP, &n, sizeof(n));
+		if (e < 0) {
+			perror("on_startup() failed");
+			close(g_io_channel_sock);
+			g_main_loop_quit(g_loop);
+			return FALSE;
+		}
+
+/* multicast ttl */
+		int ttl = 1;
+		e = setsockopt(g_io_channel_sock, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl));
+		if (e < 0) {
+			perror("on_startup() failed");
+			close(g_io_channel_sock);
+			g_main_loop_quit(g_loop);
+			return FALSE;
+		}
+
+	} /* IN_MULTICAST */
+
+/* period timer to indicate some form of life */
+// TODO: Gnome 2.14: replace with g_timeout_add_seconds()
+	g_timeout_add(10 * 1000, (GSourceFunc)on_mark, NULL);
+
+	puts ("startup complete.");
+	banana_man();
+
+	return FALSE;
+}
+
+static gboolean
+on_shutdown (
+	G_GNUC_UNUSED gpointer data
+	)
+{
+	puts ("on_shutdown()");
+
+	g_main_loop_quit (g_loop);
+	return FALSE;
+}
+
+
+/***************************************************************
+ * banana man!
+ */
+
+void
+banana_man (
+	void
+	)
+{
+	puts ("banana man.");
+
+	int e;
+
+/* construct PGM packet */
+	int tpdu_length = sizeof(struct pgm_header) + sizeof(struct pgm_data) + strlen(g_payload) + 1;
+	gchar *buf = (gchar*)malloc( tpdu_length );
+	if (buf == NULL) {
+		perror ("oh crap.");
+		return;
+	}
+
+printf ("PGM header size %" G_GSIZE_FORMAT "\n"
+	"PGM data header size %" G_GSIZE_FORMAT "\n"
+	"payload size %" G_GSIZE_FORMAT "\n",
+	sizeof(struct pgm_header),
+	sizeof(struct pgm_data),
+	strlen(g_payload) + 1);
+
+	struct pgm_header *header = (struct pgm_header*)buf;
+	struct pgm_data *odata = (struct pgm_data*)(header + 1);
+
+	header->pgm_sport	= g_htons (g_port);
+	header->pgm_dport	= g_htons (g_port);
+	header->pgm_type	= PGM_ODATA;
+	header->pgm_options	= 0;
+	header->pgm_checksum	= 0;
+
+	header->pgm_gsi[0]	= 1;
+	header->pgm_gsi[1]	= 2;
+	header->pgm_gsi[2]	= 3;
+	header->pgm_gsi[3]	= 4;
+	header->pgm_gsi[4]	= 5;
+	header->pgm_gsi[5]	= 6;
+
+	header->pgm_tsdu_length	= g_htons (strlen(g_payload) + 1);		/* transport data unit length */
+
+/* ODATA */
+	odata->data_sqn		= 0;
+	odata->data_trail	= 0;
+
+	memcpy (odata + 1, g_payload, strlen(g_payload) + 1);
+
+	header->pgm_checksum = pgm_csum_fold (pgm_csum_partial (buf, tpdu_length, 0));
+
+	int flags = MSG_CONFIRM;	/* not expecting a reply */
+
+/* IP header handled by sendto() */
+	struct sockaddr_in mc;
+	mc.sin_family		= AF_INET;
+	mc.sin_addr.s_addr	= g_mreq.imr_multiaddr.s_addr;
+	mc.sin_port		= 0;
+
+	printf("TPDU %i bytes to %s.\n", tpdu_length, inet_ntoa(mc.sin_addr));
+	e = sendto (g_io_channel_sock,
+		buf,
+		tpdu_length,
+		flags,
+		(struct sockaddr*)&mc,	/* to address */
+		sizeof(mc));		/* address size */
+	if (e == EMSGSIZE) {
+		perror ("message too large for ip stack.");
+		return;
+	}
+	if (e < 0) {
+		perror ("sendto() failed.");
+		return;
+	}
+
+	puts ("sent.");
+
+	puts ("schedule shutdown in 1000ms.");
+	g_timeout_add(1000, (GSourceFunc)on_shutdown, NULL);
+}
+
+static gboolean
+on_mark (
+	G_GNUC_UNUSED gpointer data
+	)
+{
+	g_message ("-- MARK --");
+	return TRUE;
+}
+
+/* eof */
---
+++ tags/release-1-0-0/openpgm/pgm/examples/asyncqrecv.c
@@ -0,0 +1,226 @@
+/* vim:ts=8:sts=8:sw=4:noai:noexpandtab
+ *
+ * Simple PGM receiver: callback based asynchronous queue.
+ *
+ * Copyright (c) 2006-2007 Miru Limited.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+#include <errno.h>
+#include <getopt.h>
+#include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+#include <glib.h>
+
+#include <pgm/pgm.h>
+#include <pgm/backtrace.h>
+#include <pgm/log.h>
+
+
+/* typedefs */
+
+/* globals */
+
+static int g_port = 7500;
+static const char* g_network = "";
+static int g_udp_encap_port = 0;
+
+static int g_max_tpdu = 1500;
+static int g_sqns = 100;
+
+static pgm_transport_t* g_transport = NULL;
+static GMainLoop* g_loop = NULL;
+
+static void on_signal (int);
+static gboolean on_startup (void);
+
+static int on_data (gpointer, guint, gpointer);
+
+
+G_GNUC_NORETURN static void
+usage (
+	const char*	bin
+	)
+{
+	fprintf (stderr, "Usage: %s [options]\n", bin);
+	fprintf (stderr, "  -n <network>    : Multicast group or unicast IP address\n");
+	fprintf (stderr, "  -s <port>       : IP port\n");
+	fprintf (stderr, "  -p <port>       : Encapsulate PGM in UDP on IP port\n");
+	exit (1);
+}
+
+int
+main (
+	int		argc,
+	char*		argv[]
+	)
+{
+	g_message ("async-q-recv");
+
+/* parse program arguments */
+	const char* binary_name = strrchr (argv[0], '/');
+	int c;
+	while ((c = getopt (argc, argv, "s:n:p:h")) != -1)
+	{
+		switch (c) {
+		case 'n':	g_network = optarg; break;
+		case 's':	g_port = atoi (optarg); break;
+		case 'p':	g_udp_encap_port = atoi (optarg); break;
+
+		case 'h':
+		case '?': usage (binary_name);
+		}
+	}
+
+	log_init ();
+	pgm_init ();
+
+	g_loop = g_main_loop_new (NULL, FALSE);
+
+/* setup signal handlers */
+	signal(SIGSEGV, on_sigsegv);
+	pgm_signal_install(SIGINT, on_signal);
+	pgm_signal_install(SIGTERM, on_signal);
+	pgm_signal_install(SIGHUP, SIG_IGN);
+
+/* delayed startup */
+	g_message ("scheduling startup.");
+	g_timeout_add (0, (GSourceFunc)on_startup, NULL);
+
+/* dispatch loop */
+	g_message ("entering main event loop ... ");
+	g_main_loop_run (g_loop);
+
+	g_message ("message loop terminated, cleaning up.");
+
+/* cleanup */
+	g_main_loop_unref(g_loop);
+	g_loop = NULL;
+
+	if (g_transport) {
+		g_message ("destroying transport.");
+
+		pgm_transport_destroy (g_transport, TRUE);
+		g_transport = NULL;
+	}
+
+	g_message ("finished.");
+	return 0;
+}
+
+static void
+on_signal (
+	G_GNUC_UNUSED int signum
+	)
+{
+	g_message ("on_signal");
+	g_main_loop_quit (g_loop);
+}
+
+static gboolean
+on_startup (void)
+{
+	g_message ("startup.");
+	g_message ("create transport.");
+
+	pgm_gsi_t gsi;
+	int e = pgm_create_md5_gsi (&gsi);
+	g_assert (e == 0);
+
+	struct group_source_req recv_gsr, send_gsr;
+	char network[1024];
+	sprintf (network, ";%s", g_network);
+	gsize recv_len = 1;
+	e = pgm_if_parse_transport (network, AF_INET, &recv_gsr, &recv_len, &send_gsr);
+	g_assert (e == 0);
+	g_assert (recv_len == 1);
+
+	if (g_udp_encap_port) {
+		((struct sockaddr_in*)&send_gsr.gsr_group)->sin_port = g_htons (g_udp_encap_port);
+		((struct sockaddr_in*)&recv_gsr.gsr_group)->sin_port = g_htons (g_udp_encap_port);
+	}
+
+	e = pgm_transport_create (&g_transport, &gsi, 0, g_port, &recv_gsr, 1, &send_gsr);
+	g_assert (e == 0);
+
+	pgm_transport_set_recv_only (g_transport, FALSE);
+	pgm_transport_set_max_tpdu (g_transport, g_max_tpdu);
+	pgm_transport_set_rxw_sqns (g_transport, g_sqns);
+	pgm_transport_set_hops (g_transport, 16);
+	pgm_transport_set_peer_expiry (g_transport, pgm_secs(300));
+	pgm_transport_set_spmr_expiry (g_transport, pgm_msecs(250));
+	pgm_transport_set_nak_bo_ivl (g_transport, pgm_msecs(50));
+	pgm_transport_set_nak_rpt_ivl (g_transport, pgm_secs(2));
+	pgm_transport_set_nak_rdata_ivl (g_transport, pgm_secs(2));
+	pgm_transport_set_nak_data_retries (g_transport, 50);
+	pgm_transport_set_nak_ncf_retries (g_transport, 50);
+
+	e = pgm_transport_bind (g_transport);
+	if (e < 0) {
+		if      (e == -1)
+			g_critical ("pgm_transport_bind failed errno %i: \"%s\"", errno, strerror(errno));
+		else if (e == -2)
+			g_critical ("pgm_transport_bind failed h_errno %i: \"%s\"", h_errno, hstrerror(h_errno));
+		else
+			g_critical ("pgm_transport_bind failed e %i", e);
+		g_main_loop_quit(g_loop);
+		return FALSE;
+	}
+	g_assert (e == 0);
+
+/* asynchronous receiver thread */
+	pgm_async_t* async = NULL;
+	pgm_async_create (&async, g_transport, 0);
+	pgm_async_add_watch (async, on_data, NULL);
+
+	g_message ("startup complete.");
+	return FALSE;
+}
+
+static int
+on_data (
+	gpointer	data,
+	guint		len,
+	G_GNUC_UNUSED gpointer user_data
+	)
+{
+	static struct timeval tv;
+	gettimeofday(&tv, NULL);
+
+/* protect against non-null terminated strings */
+	char buf[1024];
+	snprintf (buf, sizeof(buf), "%s", (char*)data);
+
+	g_message ("\"%s\" (%i bytes)",
+			buf,
+			len);
+
+	return 0;
+}
+
+/* eof */
