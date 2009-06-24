098a4bef302c0a71e68f2b4aaeec9317da32b5bf
Steven McCoy <fnjordy@gmail.com>
2009-06-24 03:11:04 +0000

    * valid PGM skbuff can have data point to packet or payload. * clear PGM header in testing as all fields are not set. * add tests for inline functions.

--- openpgm/pgm/txwi_unittest.c
+++ openpgm/pgm/txwi_unittest.c
@@ -6,21 +6,27 @@
 #include "txwi.c"
 
 
-/* generate valid skb */
+/* generate valid skb, data pointer pointing to PGM payload
+ */
 static
 struct pgm_sk_buff_t*
 generate_valid_skb (void)
 {
-	const guint16 tsdu_length = 0;
+	const guint16 tsdu_length = 1000;
 	const guint16 header_length = sizeof(struct pgm_header) + sizeof(struct pgm_data);
 	struct pgm_sk_buff_t* skb = pgm_alloc_skb (1500);
+/* fake but valid transport and timestamp */
 	skb->transport = (pgm_transport_t*)0x1;
 	skb->tstamp = 1;
-	pgm_skb_put (skb, header_length + tsdu_length);
-	skb->pgm_header = (struct pgm_header*)skb->data;
+/* header */
+	pgm_skb_reserve (skb, header_length);
+	memset (skb->head, 0, header_length);
+	skb->pgm_header = (struct pgm_header*)skb->head;
 	skb->pgm_data   = (struct pgm_data*)(skb->pgm_header + 1);
 	skb->pgm_header->pgm_type = PGM_ODATA;
 	skb->pgm_header->pgm_tsdu_length = g_htons (tsdu_length);
+/* DATA */
+	pgm_skb_put (skb, tsdu_length);
 	return skb;
 }
 
@@ -200,6 +206,186 @@ START_TEST (test_peek_fail_002)
 }
 END_TEST
 
+/** inline function tests **/
+/* pgm_txw_max_length () 
+ */
+START_TEST (test_max_length_pass_001)
+{
+	const guint window_length = 100;
+	pgm_txw_t* window = pgm_txw_init (0, window_length, 0, 0);
+	fail_if (NULL == window);
+	fail_unless (window_length == pgm_txw_max_length (window));
+	fail_unless (0 == pgm_txw_shutdown (window));
+}
+END_TEST
+
+START_TEST (test_max_length_fail_001)
+{
+	pgm_txw_max_length (NULL);
+	fail ();
+}
+END_TEST
+
+/* pgm_txw_length () 
+ */
+START_TEST (test_length_pass_001)
+{
+	pgm_txw_t* window = pgm_txw_init (0, 100, 0, 0);
+	fail_if (NULL == window);
+	fail_unless (0 == pgm_txw_length (window));
+	struct pgm_sk_buff_t* skb = generate_valid_skb ();
+	fail_if (NULL == skb);
+	pgm_txw_add (window, skb);
+	fail_unless (1 == pgm_txw_length (window));
+	fail_unless (0 == pgm_txw_shutdown (window));
+}
+END_TEST
+
+START_TEST (test_length_fail_001)
+{
+	pgm_txw_length (NULL);
+	fail ();
+}
+END_TEST
+
+/* pgm_txw_size () 
+ */
+START_TEST (test_size_pass_001)
+{
+	pgm_txw_t* window = pgm_txw_init (0, 100, 0, 0);
+	fail_if (NULL == window);
+	fail_unless (0 == pgm_txw_size (window));
+	struct pgm_sk_buff_t* skb = generate_valid_skb ();
+	fail_if (NULL == skb);
+	pgm_txw_add (window, skb);
+	fail_unless (1000 == pgm_txw_size (window));
+	fail_unless (0 == pgm_txw_shutdown (window));
+}
+END_TEST
+
+START_TEST (test_size_fail_001)
+{
+	pgm_txw_size (NULL);
+	fail ();
+}
+END_TEST
+
+/* pgm_txw_is_empty
+ */
+START_TEST (test_is_empty_pass_001)
+{
+	pgm_txw_t* window = pgm_txw_init (0, 100, 0, 0);
+	fail_if (NULL == window);
+	fail_unless (pgm_txw_is_empty (window));
+	struct pgm_sk_buff_t* skb = generate_valid_skb ();
+	fail_if (NULL == skb);
+	pgm_txw_add (window, skb);
+	fail_if (pgm_txw_is_empty (window));
+	fail_unless (0 == pgm_txw_shutdown (window));
+}
+END_TEST
+
+START_TEST (test_is_empty_fail_001)
+{
+	pgm_txw_is_empty (NULL);
+	fail ();
+}
+END_TEST
+
+/* pgm_txw_is_full
+ */
+START_TEST (test_is_full_pass_001)
+{
+	pgm_txw_t* window = pgm_txw_init (0, 1, 0, 0);
+	fail_if (NULL == window);
+	fail_if (pgm_txw_is_full (window));
+	struct pgm_sk_buff_t* skb = generate_valid_skb ();
+	fail_if (NULL == skb);
+	pgm_txw_add (window, skb);
+	fail_unless (pgm_txw_is_full (window));
+	fail_unless (0 == pgm_txw_shutdown (window));
+}
+END_TEST
+
+START_TEST (test_is_full_fail_001)
+{
+	pgm_txw_is_full (NULL);
+	fail ();
+}
+END_TEST
+
+/* pgm_txw_lead
+ */
+START_TEST (test_lead_pass_001)
+{
+	pgm_txw_t* window = pgm_txw_init (0, 100, 0, 0);
+	fail_if (NULL == window);
+	guint32 lead = pgm_txw_lead (window);
+	struct pgm_sk_buff_t* skb = generate_valid_skb ();
+	fail_if (NULL == skb);
+	pgm_txw_add (window, skb);
+	fail_unless (lead + 1 == pgm_txw_lead (window));
+	fail_unless (0 == pgm_txw_shutdown (window));
+}
+END_TEST
+
+START_TEST (test_lead_fail_001)
+{
+	pgm_txw_lead (NULL);
+	fail ();
+}
+END_TEST
+
+/* pgm_txw_next_lead
+ */
+START_TEST (test_next_lead_pass_001)
+{
+	const guint window_length = 100;
+	pgm_txw_t* window = pgm_txw_init (0, window_length, 0, 0);
+	fail_if (NULL == window);
+	guint32 next_lead = pgm_txw_next_lead (window);
+	struct pgm_sk_buff_t* skb = generate_valid_skb ();
+	fail_if (NULL == skb);
+	pgm_txw_add (window, skb);
+	fail_unless (next_lead == pgm_txw_lead (window));
+	fail_unless (0 == pgm_txw_shutdown (window));
+}
+END_TEST
+
+START_TEST (test_next_lead_fail_001)
+{
+	pgm_txw_next_lead (NULL);
+	fail ();
+}
+END_TEST
+
+/* pgm_txw_trail
+ */
+START_TEST (test_trail_pass_001)
+{
+	pgm_txw_t* window = pgm_txw_init (0, 1, 0, 0);
+	fail_if (NULL == window);
+/* does not advance with adding skb */
+	guint32 trail = pgm_txw_trail (window);
+	struct pgm_sk_buff_t* skb = generate_valid_skb ();
+	fail_if (NULL == skb);
+	pgm_txw_add (window, skb);
+	fail_unless (trail == pgm_txw_trail (window));
+/* does advance when filling up window */
+	skb = generate_valid_skb ();
+	fail_if (NULL == skb);
+	pgm_txw_add (window, skb);
+	fail_if (trail == pgm_txw_trail (window));
+	fail_unless (0 == pgm_txw_shutdown (window));
+}
+END_TEST
+
+START_TEST (test_trail_fail_001)
+{
+	pgm_txw_trail (NULL);
+	fail ();
+}
+END_TEST
 
 static
 Suite*
@@ -240,27 +426,43 @@ make_test_suite (void)
 
 	TCase* tc_max_length = tcase_create ("max-length");
 	suite_add_tcase (s, tc_max_length);
+	tcase_add_test (tc_max_length, test_max_length_pass_001);
+	tcase_add_test_raise_signal (tc_max_length, test_max_length_fail_001, SIGABRT);
 
 	TCase* tc_length = tcase_create ("length");
 	suite_add_tcase (s, tc_length);
+	tcase_add_test (tc_length, test_length_pass_001);
+	tcase_add_test_raise_signal (tc_length, test_length_fail_001, SIGABRT);
 
 	TCase* tc_size = tcase_create ("size");
 	suite_add_tcase (s, tc_size);
+	tcase_add_test (tc_size, test_size_pass_001);
+	tcase_add_test_raise_signal (tc_size, test_size_fail_001, SIGABRT);
 
 	TCase* tc_is_empty = tcase_create ("is-empty");
 	suite_add_tcase (s, tc_is_empty);
+	tcase_add_test (tc_is_empty, test_is_empty_pass_001);
+	tcase_add_test_raise_signal (tc_is_empty, test_is_empty_fail_001, SIGABRT);
 
 	TCase* tc_is_full = tcase_create ("is-full");
 	suite_add_tcase (s, tc_is_full);
-
-	TCase* tc_next_lead = tcase_create ("next-lead");
-	suite_add_tcase (s, tc_next_lead);
+	tcase_add_test (tc_is_full, test_is_full_pass_001);
+	tcase_add_test_raise_signal (tc_is_full, test_is_full_fail_001, SIGABRT);
 
 	TCase* tc_lead = tcase_create ("lead");
 	suite_add_tcase (s, tc_lead);
+	tcase_add_test (tc_lead, test_lead_pass_001);
+	tcase_add_test_raise_signal (tc_lead, test_lead_fail_001, SIGABRT);
+
+	TCase* tc_next_lead = tcase_create ("next-lead");
+	suite_add_tcase (s, tc_next_lead);
+	tcase_add_test (tc_next_lead, test_next_lead_pass_001);
+	tcase_add_test_raise_signal (tc_next_lead, test_next_lead_fail_001, SIGABRT);
 
 	TCase* tc_trail = tcase_create ("trail");
 	suite_add_tcase (s, tc_trail);
+	tcase_add_test (tc_trail, test_trail_pass_001);
+	tcase_add_test_raise_signal (tc_trail, test_trail_fail_001, SIGABRT);
 
 	TCase* tc_retransmit_push = tcase_create ("retransmit-push");
 	suite_add_tcase (s, tc_retransmit_push);
