a3d6b7c79ab92bdcbea93f4932d78c5ff79edeb5
Steven McCoy <fnjordy@gmail.com>
2010-02-02 07:29:00 +0000

    Add INSTALL note.

--- openpgm/pgm/sockaddr.c
+++ openpgm/pgm/sockaddr.c
@@ -2,7 +2,7 @@
  * 
  * struct sockaddr functions independent of in or in6.
  *
- * Copyright (c) 2006-2009 Miru Limited.
+ * Copyright (c) 2006-2010 Miru Limited.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
