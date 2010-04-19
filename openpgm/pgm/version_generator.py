021131c9a6f206002c726bd37e3464c8239b3831
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2010-04-19 09:52:35 +0000

    * Add extra checking to pgm_transport_bind(). * Update transport unit tests for v3.

--- openpgm/pgm/version_generator.py
+++ openpgm/pgm/version_generator.py
@@ -38,7 +38,7 @@ print """
 
 const unsigned pgm_major_version = 3;
 const unsigned pgm_minor_version = 0;
-const unsigned pgm_micro_version = 35;
+const unsigned pgm_micro_version = 36;
 const char* pgm_build_date = "%s";
 const char* pgm_build_time = "%s";
 const char* pgm_build_platform = "%s";
