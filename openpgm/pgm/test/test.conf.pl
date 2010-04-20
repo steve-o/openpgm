86638975cbd5601c2929d0fcfd55719b1249bed5
Steven McCoy <fnjordy@gmail.com>
2010-04-20 03:51:19 +0000

    If required, initialise GLib threads inside the asynchronous receiver startup.

--- openpgm/pgm/test/test.conf.pl
+++ openpgm/pgm/test/test.conf.pl
@@ -7,10 +7,10 @@ use vars qw ( %config );
 		ip	=> '10.6.28.34',
 	},
 	app => {
-#		host	=> 'ayaka',	ip	=> '10.6.28.31',
-#		cmd	=> '/miru/projects/openpgm/pgm/ref/debug/test/app',
-		host	=> 'ryoko',	ip	=> '10.6.28.36',
-		cmd	=> 'LD_LIBRARY_PATH=/opt/glib-sunstudio/lib:$LD_LIBRARY_PATH /miru/projects/openpgm/pgm/ref/debug/test/app',
+		host	=> 'ayaka',	ip	=> '10.6.28.31',
+		cmd	=> '/miru/projects/openpgm/pgm/ref/debug/test/app',
+#		host	=> 'ryoko',	ip	=> '10.6.28.36',
+#		cmd	=> 'LD_LIBRARY_PATH=/opt/glib-sunstudio/lib:$LD_LIBRARY_PATH /miru/projects/openpgm/pgm/ref/debug/test/app',
 	},
 	mon => {
 		host	=> 'sora',
