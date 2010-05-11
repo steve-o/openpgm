60c961a133c4fdfcaae62501c3ba5ebb14be5a94
Steven McCoy <fnjordy@gmail.com>
2010-05-11 08:20:22 +0000

    * New API, pgm_transport_set_router_alert() to enable disable RFC 2113 usage. * Remove unrequired start case for SPM sequences. * Fix missing sequence in transmit window trace.

--- openpgm/pgm/transport.c
+++ openpgm/pgm/transport.c
@@ -382,25 +382,29 @@ pgm_transport_create (
 #endif
 		goto err_destroy;
 	}
-	pgm_trace (PGM_LOG_ROLE_NETWORK,_("Request IP Router Alert (RFC 2113)."));
-	if (0 != pgm_sockaddr_router_alert (new_transport->send_with_router_alert_sock, new_transport->send_gsr.gsr_group.ss_family, TRUE))
+
+	if (new_transport->use_router_alert)
 	{
+		pgm_trace (PGM_LOG_ROLE_NETWORK,_("Request IP Router Alert (RFC 2113)."));
+		if (0 != pgm_sockaddr_router_alert (new_transport->send_with_router_alert_sock, new_transport->send_gsr.gsr_group.ss_family, TRUE))
+		{
 #ifndef _WIN32
-		const int save_errno = errno;
-		pgm_set_error (error,
-			     PGM_ERROR_DOMAIN_TRANSPORT,
-			     pgm_error_from_errno (save_errno),
-			     _("Enabling IP Router Alert (RFC 2113): %s"),
-			     strerror (save_errno));
+			const int save_errno = errno;
+			pgm_set_error (error,
+				       PGM_ERROR_DOMAIN_TRANSPORT,
+				       pgm_error_from_errno (save_errno),
+				       _("Enabling IP Router Alert (RFC 2113): %s"),
+				       strerror (save_errno));
 #else
-		const int save_errno = WSAGetLastError();
-		pgm_set_error (error,
-			     PGM_ERROR_DOMAIN_TRANSPORT,
-			     pgm_error_from_wsa_errno (save_errno),
-			     _("Enabling IP Router Alert (RFC 2113): %s"),
-			     pgm_wsastrerror (save_errno));
+			const int save_errno = WSAGetLastError();
+			pgm_set_error (error,
+				       PGM_ERROR_DOMAIN_TRANSPORT,
+				       pgm_error_from_wsa_errno (save_errno),
+				       _("Enabling IP Router Alert (RFC 2113): %s"),
+				       pgm_wsastrerror (save_errno));
 #endif
-		goto err_destroy;
+			goto err_destroy;
+		}
 	}
 
 	*transport = new_transport;
@@ -1821,6 +1825,32 @@ pgm_transport_set_congestion_control (
 	}
 	transport->use_pgmcc	= use_pgmcc;
 	transport->acker_ivl	= pgm_msecs (acker_ivl);
+	pgm_rwlock_reader_unlock (&transport->lock);
+	return TRUE;
+}
+
+/* send IP Router Alert on IP/PGM packets as required, some networking infrastructure
+ * may silently consume RFC 2113 packets breaking PGM.
+ *
+ * on success, returns TRUE, on failure returns FALSE.
+ */
+
+bool
+pgm_transport_set_router_alert (
+	pgm_transport_t* const	transport,
+	const bool		use_ip_router_alert
+	)
+{
+	pgm_return_val_if_fail (transport != NULL, FALSE);
+	if (!pgm_rwlock_reader_trylock (&transport->lock))
+		pgm_return_val_if_reached (FALSE);
+	if (transport->is_bound ||
+	    transport->is_destroyed)
+	{
+		pgm_rwlock_reader_unlock (&transport->lock);
+		pgm_return_val_if_reached (FALSE);
+	}
+	transport->use_router_alert	= use_ip_router_alert;
 	pgm_rwlock_reader_unlock (&transport->lock);
 	return TRUE;
 }
