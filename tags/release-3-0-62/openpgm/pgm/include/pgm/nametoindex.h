f55cb556527527aa037df2c403aa562cf5f4b46c
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2010-06-01 09:23:39 +0000

    tagging release 3.0.62

---
+++ tags/release-3-0-62/openpgm/pgm/include/pgm/nametoindex.h
@@ -0,0 +1,37 @@
+/* vim:ts=8:sts=8:sw=4:noai:noexpandtab
+ *
+ * Windows interface name to interface index function.
+ *
+ * Copyright (c) 2006-2009 Miru Limited.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#if !defined (__PGM_FRAMEWORK_H_INSIDE__) && !defined (PGM_COMPILATION)
+#       error "Only <framework.h> can be included directly."
+#endif
+
+#ifndef __PGM_NAMETOINDEX_H__
+#define __PGM_NAMETOINDEX_H__
+
+#include <pgm/types.h>
+
+PGM_BEGIN_DECLS
+
+unsigned pgm_if_nametoindex (const sa_family_t, const char*);
+
+PGM_END_DECLS
+
+#endif /* __PGM_NAMETOINDEX_H__ */
---
+++ tags/release-3-0-62/openpgm/pgm/include/pgm/queue.h
@@ -0,0 +1,51 @@
+/* vim:ts=8:sts=8:sw=4:noai:noexpandtab
+ *
+ * portable double-ended queue.
+ *
+ * Copyright (c) 2010 Miru Limited.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#if !defined (__PGM_FRAMEWORK_H_INSIDE__) && !defined (PGM_COMPILATION)
+#	error "Only <framework.h> can be included directly."
+#endif
+
+#ifndef __PGM_QUEUE_H__
+#define __PGM_QUEUE_H__
+
+#include <pgm/macros.h>
+#include <pgm/list.h>
+
+PGM_BEGIN_DECLS
+
+struct pgm_queue_t
+{
+	pgm_list_t*	head;		/* head & tail equal on 1 element */
+	pgm_list_t*	tail;
+	unsigned	length;
+};
+
+typedef struct pgm_queue_t pgm_queue_t;
+
+bool pgm_queue_is_empty (const pgm_queue_t*const) PGM_GNUC_WARN_UNUSED_RESULT;
+void pgm_queue_push_head_link (pgm_queue_t*restrict, pgm_list_t*restrict);
+pgm_list_t* pgm_queue_pop_tail_link (pgm_queue_t*);
+pgm_list_t* pgm_queue_peek_tail_link (pgm_queue_t*) PGM_GNUC_WARN_UNUSED_RESULT;
+void pgm_queue_unlink (pgm_queue_t*restrict, pgm_list_t*restrict);
+
+PGM_END_DECLS
+
+#endif /* __PGM_QUEUE_H__ */
