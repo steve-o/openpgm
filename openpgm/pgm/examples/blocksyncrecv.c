1594fcbb76e42083b3ed2f6b40599a2a96cc1623
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2010-06-08 04:50:57 +0000

    * Update example library for changed headers. * Remove PGMCC from purinsend. * Update blocksyncrecv GLib example.

--- openpgm/pgm/examples/blocksyncrecv.c
+++ openpgm/pgm/examples/blocksyncrecv.c
@@ -22,8 +22,11 @@
 #include <locale.h>
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <glib.h>
-#ifdef G_OS_WIN32
+#ifndef G_OS_WIN32
+#	include <unistd.h>
+#else
 #	include "getopt.h"
 #endif
 #include <pgm/pgm.h>
@@ -45,7 +48,7 @@ static int		g_udp_encap_port = 0;
 static int		g_max_tpdu = 1500;
 static int		g_sqns = 100;
 
-static pgm_transport_t* g_transport = NULL;
+static pgm_sock_t*	g_sock = NULL;
 static gboolean		g_quit = FALSE;
 
 #ifdef G_OS_UNIX
@@ -54,10 +57,11 @@ static void on_signal (int);
 static BOOL on_console_ctrl (DWORD);
 #endif
 static gboolean on_startup (void);
-static int on_data (gpointer, guint, pgm_tsi_t*);
+static int on_data (gconstpointer, size_t, struct pgm_sockaddr_t*);
 
 
-G_GNUC_NORETURN static void
+G_GNUC_NORETURN static
+void
 usage (
 	const char*	bin
 	)
@@ -122,15 +126,17 @@ main (
 /* dispatch loop */
 	g_message ("entering PGM message loop ... ");
 	do {
-		pgm_tsi_t from;
 		char buffer[4096];
-		gsize len;
-		const int status = pgm_recvfrom (g_transport,
+		size_t len;
+		struct pgm_sockaddr_t from;
+		socklen_t fromlen = sizeof(from);
+		const int status = pgm_recvfrom (g_sock,
 					         buffer,
 					         sizeof(buffer),
 					         0,
 					         &len,
 					         &from,
+						 &fromlen,
 					         &pgm_err);
 		if (PGM_IO_STATUS_NORMAL == status)
 			on_data (buffer, len, &from);
@@ -148,10 +154,10 @@ main (
 	g_message ("message loop terminated, cleaning up.");
 
 /* cleanup */
-	if (g_transport) {
-		g_message ("destroying transport.");
-		pgm_transport_destroy (g_transport, TRUE);
-		g_transport = NULL;
+	if (g_sock) {
+		g_message ("closing PGM socket.");
+		pgm_close (g_sock, TRUE);
+		g_sock = NULL;
 	}
 
 	g_message ("PGM engine shutdown.");
@@ -161,7 +167,8 @@ main (
 }
 
 #ifdef G_OS_UNIX
-static void
+static
+void
 on_signal (
 	int		signum
 	)
@@ -182,86 +189,160 @@ on_console_ctrl (
 }
 #endif /* !G_OS_UNIX */
 
-static gboolean
+static
+gboolean
 on_startup (void)
 {
-	struct pgm_transport_info_t* res = NULL;
+	struct pgm_addrinfo_t* res = NULL;
 	pgm_error_t* pgm_err = NULL;
+	sa_family_t sa_family = AF_UNSPEC;
 
 	g_message ("startup.");
-	g_message ("create transport.");
+	g_message ("create PGM socket.");
 
 /* parse network parameter into transport address structure */
-	char network[1024];
-	sprintf (network, "%s", g_network);
-	if (!pgm_if_get_transport_info (network, NULL, &res, &pgm_err)) {
+	if (!pgm_getaddrinfo (g_network, NULL, &res, &pgm_err)) {
 		g_error ("parsing network parameter: %s", pgm_err->message);
-		pgm_error_free (pgm_err);
-		return FALSE;
+		goto err_abort;
 	}
-/* create global session identifier */
-	if (!pgm_gsi_create_from_hostname (&res->ti_gsi, &pgm_err)) {
-		g_error ("creating GSI: %s", pgm_err->message);
-		pgm_error_free (pgm_err);
-		pgm_if_free_transport_info (res);
-		return FALSE;
-	}
+
+	sa_family = res->ai_send_addrs[0].gsr_group.ss_family;
+
 	if (g_udp_encap_port) {
-		res->ti_udp_encap_ucast_port = g_udp_encap_port;
-		res->ti_udp_encap_mcast_port = g_udp_encap_port;
+		g_message ("create PGM/UDP socket.");
+		if (!pgm_socket (&g_sock, sa_family, SOCK_SEQPACKET, IPPROTO_UDP, &pgm_err)) {
+			g_error ("socket: %s", pgm_err->message);
+			goto err_abort;
+		}
+		pgm_setsockopt (g_sock, PGM_UDP_ENCAP_UCAST_PORT, &g_udp_encap_port, sizeof(g_udp_encap_port));
+		pgm_setsockopt (g_sock, PGM_UDP_ENCAP_MCAST_PORT, &g_udp_encap_port, sizeof(g_udp_encap_port));
+	} else {
+		g_message ("create PGM/IP socket.");
+		if (!pgm_socket (&g_sock, sa_family, SOCK_SEQPACKET, IPPROTO_PGM, &pgm_err)) {
+			g_error ("socket: %s", pgm_err->message);
+			goto err_abort;
+		}
 	}
-	if (g_port)
-		res->ti_dport = g_port;
-	if (!pgm_transport_create (&g_transport, res, &pgm_err)) {
-		g_error ("creating transport: %s", pgm_err->message);
-		pgm_error_free (pgm_err);
-		pgm_if_free_transport_info (res);
-		return FALSE;
-	}
-	pgm_if_free_transport_info (res);
+
+/* Use RFC 2113 tagging for PGM Router Assist */
+	const int no_router_assist = 0;
+	pgm_setsockopt (g_sock, PGM_IP_ROUTER_ALERT, &no_router_assist, sizeof(no_router_assist));
+
+	pgm_drop_superuser();
 
 /* set PGM parameters */
-	pgm_transport_set_nonblocking (g_transport, FALSE);	/* blocking */
-	pgm_transport_set_recv_only (g_transport, TRUE, FALSE);
-	pgm_transport_set_max_tpdu (g_transport, g_max_tpdu);
-	pgm_transport_set_rxw_sqns (g_transport, g_sqns);
-	pgm_transport_set_hops (g_transport, 16);
-	pgm_transport_set_peer_expiry (g_transport, pgm_secs(300));
-	pgm_transport_set_spmr_expiry (g_transport, pgm_msecs(250));
-	pgm_transport_set_nak_bo_ivl (g_transport, pgm_msecs(50));
-	pgm_transport_set_nak_rpt_ivl (g_transport, pgm_secs(2));
-	pgm_transport_set_nak_rdata_ivl (g_transport, pgm_secs(2));
-	pgm_transport_set_nak_data_retries (g_transport, 50);
-	pgm_transport_set_nak_ncf_retries (g_transport, 50);
+	const int recv_only = 1,
+		  passive = 0,
+		  peer_expiry = pgm_secs (300),
+		  spmr_expiry = pgm_msecs (250),
+		  nak_bo_ivl = pgm_msecs (50),
+		  nak_rpt_ivl = pgm_secs (2),
+		  nak_rdata_ivl = pgm_secs (2),
+		  nak_data_retries = 50,
+		  nak_ncf_retries = 50;
 
-/* assign transport to specified address */
-	if (!pgm_transport_bind (g_transport, &pgm_err)) {
-		g_error ("binding transport: %s", pgm_err->message);
-		pgm_error_free (pgm_err);
-		pgm_transport_destroy (g_transport, FALSE);
-		g_transport = NULL;
-		return FALSE;
+	pgm_setsockopt (g_sock, PGM_RECV_ONLY, &recv_only, sizeof(recv_only));
+	pgm_setsockopt (g_sock, PGM_PASSIVE, &passive, sizeof(passive));
+	pgm_setsockopt (g_sock, PGM_MTU, &g_max_tpdu, sizeof(g_max_tpdu));
+	pgm_setsockopt (g_sock, PGM_RXW_SQNS, &g_sqns, sizeof(g_sqns));
+	pgm_setsockopt (g_sock, PGM_PEER_EXPIRY, &peer_expiry, sizeof(peer_expiry));
+	pgm_setsockopt (g_sock, PGM_SPMR_EXPIRY, &spmr_expiry, sizeof(spmr_expiry));
+	pgm_setsockopt (g_sock, PGM_NAK_BO_IVL, &nak_bo_ivl, sizeof(nak_bo_ivl));
+	pgm_setsockopt (g_sock, PGM_NAK_RPT_IVL, &nak_rpt_ivl, sizeof(nak_rpt_ivl));
+	pgm_setsockopt (g_sock, PGM_NAK_RDATA_IVL, &nak_rdata_ivl, sizeof(nak_rdata_ivl));
+	pgm_setsockopt (g_sock, PGM_NAK_DATA_RETRIES, &nak_data_retries, sizeof(nak_data_retries));
+	pgm_setsockopt (g_sock, PGM_NAK_NCF_RETRIES, &nak_ncf_retries, sizeof(nak_ncf_retries));
+
+/* create global session identifier */
+	struct pgm_sockaddr_t addr;
+	memset (&addr, 0, sizeof(addr));
+	addr.sa_port = g_port ? g_port : DEFAULT_DATA_DESTINATION_PORT;
+	addr.sa_addr.sport = DEFAULT_DATA_SOURCE_PORT;
+	if (!pgm_gsi_create_from_hostname (&addr.sa_addr.gsi, &pgm_err)) {
+		g_error ("creating GSI: %s", pgm_err->message);
+		goto err_abort;
+	}
+
+/* assign socket to specified address */
+	struct pgm_interface_req_t if_req;
+	memset (&if_req, 0, sizeof(if_req));
+	if_req.ir_interface = res->ai_recv_addrs[0].gsr_interface;
+	if_req.ir_scope_id  = 0;
+	if (AF_INET6 == sa_family) {
+		struct sockaddr_in6 sa6;
+		memcpy (&sa6, &res->ai_recv_addrs[0].gsr_group, sizeof(sa6));
+		if_req.ir_scope_id = sa6.sin6_scope_id;
+	}
+	if (!pgm_bind3 (g_sock,
+			&addr, sizeof(addr),
+			&if_req, sizeof(if_req),	/* tx interface */
+			&if_req, sizeof(if_req),	/* rx interface */
+			&pgm_err))
+	{
+		g_error ("binding PGM socket: %s", pgm_err->message);
+		goto err_abort;
+	}
+
+/* join IP multicast groups */
+	for (unsigned i = 0; i < res->ai_recv_addrs_len; i++)
+		pgm_setsockopt (g_sock, PGM_JOIN_GROUP, &res->ai_recv_addrs[i], sizeof(struct group_req));
+	pgm_setsockopt (g_sock, PGM_SEND_GROUP, &res->ai_send_addrs[0], sizeof(struct group_req));
+	pgm_freeaddrinfo (res);
+
+/* set IP parameters */
+	const int blocking = 0,
+		  multicast_loop = g_multicast_loop ? 1 : 0,
+		  multicast_hops = 16,
+		  dscp = 0x2e << 2;		/* Expedited Forwarding PHB for network elements, no ECN. */
+
+	pgm_setsockopt (g_sock, PGM_MULTICAST_LOOP, &multicast_loop, sizeof(multicast_loop));
+	pgm_setsockopt (g_sock, PGM_MULTICAST_HOPS, &multicast_hops, sizeof(multicast_hops));
+	if (AF_INET6 != sa_family)
+		pgm_setsockopt (g_sock, PGM_TOS, &dscp, sizeof(dscp));
+	pgm_setsockopt (g_sock, PGM_NOBLOCK, &blocking, sizeof(blocking));
+
+	if (!pgm_connect (g_sock, &pgm_err)) {
+		g_error ("connecting PGM socket: %s", pgm_err->message);
+		goto err_abort;
 	}
 
 	g_message ("startup complete.");
+	return TRUE;
+
+err_abort:
+	if (NULL != g_sock) {
+		pgm_close (g_sock, FALSE);
+		g_sock = NULL;
+	}
+	if (NULL != res) {
+		pgm_freeaddrinfo (res);
+		res = NULL;
+	}
+	if (NULL != pgm_err) {
+		pgm_error_free (pgm_err);
+		pgm_err = NULL;
+	}
 	return FALSE;
 }
 
-static int
+static
+int
 on_data (
-	gpointer	data,
-	guint		len,
-	pgm_tsi_t*      from
+	gconstpointer		data,
+	size_t			len,
+	struct pgm_sockaddr_t*	from
 	)
 {
 /* protect against non-null terminated strings */
 	char buf[1024], tsi[PGM_TSISTRLEN];
-	snprintf (buf, sizeof(buf), "%s", (char*)data);
-	pgm_tsi_print_r (from, tsi, sizeof(tsi));
+	const size_t buflen = MIN( sizeof(buf) - 1, len );
+	strncpy (buf, data, buflen);
+	buf[buflen] = '\0';
+	pgm_tsi_print_r (&from->sa_addr, tsi, sizeof(tsi));
 
-	g_message ("\"%s\" (%i bytes from %s)",
+	g_message ("\"%s\" (%u bytes from %s)",
 			buf,
-			len,
+			(unsigned)len,
 			tsi);
 
 	return 0;
