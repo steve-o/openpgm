5dde3a0236d1a50a6fd162135a39bf7a1650624e
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2010-07-03 12:47:19 +0000

    Define _BSD_SOURCE for IFF_UP when using Glib & Check.

--- trunk/openpgm/pgm/if_unittest.c
+++ trunk/openpgm/pgm/if_unittest.c
@@ -21,6 +21,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+/* IFF_UP */
+#define _BSD_SOURCE
+
 #include <errno.h>
 #include <netdb.h>
 #include <signal.h>
