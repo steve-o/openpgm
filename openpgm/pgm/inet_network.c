70b2ecef833bbfb6f82cfbd3fe10a0cccc2e2285
Steven McCoy <fnjordy@gmail.com>
2010-10-14 08:13:36 +0000

    * Add two new internal API: pgm_inet_lnaof and pgm_inet6_lnaof equivalent to inet_lnaof(). * Only compare network addresses when local network address is null. * Count matching network addresses and abort when more than one is found, e.g. fe80::/64. * Clean up inet_network.c for portability, simplify exit state. * Update pgm_inet6_network to return inaddr6_none (albeit undefined, use equivalent to inaddr_none). * Add one new internal API: pgm_sa6_network, a sockaddr_in version of pgm_inet6_network that stores the IPv6 scope identifier.  Can be used to update parse_interface in if.c to allow device specifications of the form "fe80::%eth0/64", i.e. generic link local scope network.

--- openpgm/pgm/inet_network.c
+++ openpgm/pgm/inet_network.c
@@ -64,15 +64,15 @@ pgm_inet_network (
 	struct in_addr*	restrict in
 	)
 {
+	const char	*p = s;
+	unsigned	 val = 0;
+	int		 shift = 24;
+
 	pgm_return_val_if_fail (NULL != s,  -1);
 	pgm_return_val_if_fail (NULL != in, -1);
 
 	pgm_debug ("pgm_inet_network (s:\"%s\" in:%p)",
 		 s, (const void*)in);
-
-	const char *p = s;
-	unsigned val = 0;
-	int shift = 24;
 
 	in->s_addr = INADDR_ANY;
 
@@ -81,43 +81,33 @@ pgm_inet_network (
 		if (isdigit (*p)) {
 			val = 10 * val + (*p - '0');
 		} else if (*p == '.' || *p == 0) {
-			if (val > 0xff) {
-				in->s_addr = INADDR_NONE;
-				return -1;
-			}
+			if (val > 0xff)
+				goto default_none;
 
 //g_trace ("elem %i", val);
 			
 			in->s_addr |= val << shift;
 			val = 0;
 			shift -= 8;
-			if (shift < 0 && *p != 0) {
-				in->s_addr = INADDR_NONE;
-				return -1;
-			}
+			if (shift < 0 && *p != 0)
+				goto default_none;
 
 		} else if (*p == '/') {
-			if (val > 0xff) {
-				in->s_addr = INADDR_NONE;
-				return -1;
-			}
+			if (val > 0xff)
+				goto default_none;
 //g_trace ("elem %i", val);
 			in->s_addr |= val << shift;
 			p++; val = 0;
 			while (*p)
 			{
-				if (isdigit (*p)) {
+				if (isdigit (*p))
 					val = 10 * val + (*p - '0');
-				} else {
-					in->s_addr = INADDR_NONE;
-					return -1;
-				}
+				else
+					goto default_none;
 				p++;
 			}
-			if (val == 0 || val > 32) {
-				in->s_addr = INADDR_NONE;
-				return -1;
-			}
+			if (val == 0 || val > 32)
+				goto default_none;
 //g_trace ("bit mask %i", val);
 
 /* zero out host bits */
@@ -131,20 +121,21 @@ g_debug ("netaddr %s", inet_ntoa (netaddr));
 			return 0;
 		
 		} else if (*p == 'x' || *p == 'X') {	/* skip number, e.g. 1.x.x.x */
-			if (val > 0) {	
-				in->s_addr = INADDR_NONE;
-				return -1;
-			}
+			if (val > 0)
+				goto default_none;
 			
 		} else {
-			in->s_addr = INADDR_NONE;
-			return -1;
+			goto default_none;
 		}
 		p++;
 	}
 
 	in->s_addr |= val << shift;
 	return 0;
+
+default_none:
+	in->s_addr = INADDR_NONE;
+	return -1;
 }
 
 /* Converts a numbers-and-dots notation string into an IPv6 network number.
@@ -162,6 +153,11 @@ pgm_inet6_network (
 	struct in6_addr* restrict in6
 	)
 {
+	const char	*p = s;
+	char		 s2[INET6_ADDRSTRLEN];
+	char		*p2 = s2;
+	unsigned	 val = 0;
+
 	pgm_return_val_if_fail (NULL != s,   -1);
 	pgm_return_val_if_fail (NULL != in6, -1);
 
@@ -174,9 +170,16 @@ pgm_inet6_network (
  * as we are dealing with network addresses IPv6 zone indices are not important
  * so we can use the inet_xtoy functions.
  */
-	char s2[INET6_ADDRSTRLEN];
-	const char *p = s;
-	char* p2 = s2;
+/* first check for scope identifier and return failure */
+	while (*p) {
+		if (*p == '%') {
+			pgm_debug ("pgm_inet_pton(AF_INET6) failed due to presence of scope identifier.");
+			goto default_none;
+		}
+		p++;
+	}
+
+	p = s;
 	while (*p) {
 		if (*p == '/') break;
 		*p2++ = *p++;
@@ -184,8 +187,7 @@ pgm_inet6_network (
 	if (*p == 0) {
 		if (pgm_inet_pton (AF_INET6, s, in6)) return 0;
 		pgm_debug ("pgm_inet_pton(AF_INET6) failed on '%s'", s);
-		memcpy (in6, &in6addr_any, sizeof(in6addr_any));
-		return -1;
+		goto default_none;
 	}
 
 	*p2 = 0;
@@ -193,7 +195,7 @@ pgm_inet6_network (
 	if (!pgm_inet_pton (AF_INET6, s2, in6)) {
 		pgm_debug ("pgm_inet_pton(AF_INET) failed parsing network part '%s'", s2);
 		memcpy (in6, &in6addr_any, sizeof(in6addr_any));
-		return -1;
+		goto default_none;
 	}
 
 #ifdef INET_NETWORK_DEBUG
@@ -202,22 +204,19 @@ pgm_inet6_network (
 #endif
 
 	p++;
-	unsigned val = 0;
 	while (*p)
 	{
 		if (isdigit(*p)) {
 			val = 10 * val + (*p - '0');
 		} else {
 			pgm_debug ("failed parsing subnet size due to character '%c'", *p);
-			memcpy (in6, &in6addr_any, sizeof(in6addr_any));
-			return -1;
+			goto default_none;
 		}
 		p++;
 	}
 	if (val == 0 || val > 128) {
 		pgm_debug ("subnet size invalid (%d)", val);
-		memcpy (in6, &in6addr_any, sizeof(in6addr_any));
-		return -1;
+		goto default_none;
 	}
 	pgm_debug ("subnet size %i", val);
 
@@ -228,9 +227,110 @@ pgm_inet6_network (
 		in6->s6_addr[ j ] &= i >= 8 ? 0x00 : (unsigned)(( 0xffU << i ) & 0xffU );
 	}
 
-	pgm_debug ("effective IPv6 network address after subnet mask: %s", pgm_inet_ntop(AF_INET6, in6, s2, sizeof(s2)));
-
+	pgm_debug ("effective IPv6 network address after subnet mask: %s",
+		pgm_inet_ntop(AF_INET6, in6, s2, sizeof(s2)));
 	return 0;
+
+default_none:
+	memset (in6, 0xff, sizeof(*in6));	/* equivalent to IN6ADDR_NONE */
+	return -1;
+}
+
+/* sockaddr_in6 version of pgm_inet6_network, we use sockaddr in order to
+ * resolve and keep the scope identifier.
+ */
+int
+pgm_sa6_network (
+	const char*	     restrict s,		/* NULL terminated */
+	struct sockaddr_in6* restrict sa6
+	)
+{
+	const char	*p = s;
+	char		 s2[INET6_ADDRSTRLEN];
+	char		*p2 = s2;
+	unsigned	 val = 0;
+	struct addrinfo	 hints = {
+		.ai_family	= AF_INET6,
+		.ai_socktype	= SOCK_STREAM,		/* not really */
+		.ai_protocol	= IPPROTO_TCP,		/* not really */
+		.ai_flags	= AI_NUMERICHOST
+	}, *result = NULL;
+
+	pgm_return_val_if_fail (NULL != s,   -1);
+	pgm_return_val_if_fail (NULL != sa6, -1);
+
+	pgm_debug ("pgm_sa6_network (s:\"%s\" sa6:%p)",
+		 s, (const void*)sa6);
+
+/* getaddrinfo cannot parse IPv6 addresses with subnet declarations, so
+ * chop them off.
+ */
+	p = s;
+	while (*p) {
+		if (*p == '/') break;
+		*p2++ = *p++;
+	}
+	if (*p == 0) {
+		if (0 == getaddrinfo (s, NULL, &hints, &result)) {
+			memcpy (sa6, result->ai_addr, result->ai_addrlen);
+			freeaddrinfo (result);
+			return 0;
+		}
+		pgm_debug ("getaddrinfo(AF_INET6) failed on '%s'", s);
+		goto default_none;
+	}
+
+	*p2 = 0;
+	pgm_debug ("net part %s", s2);
+	if (0 != getaddrinfo (s2, NULL, &hints, &result)) {
+		pgm_debug ("getaddrinfo(AF_INET) failed parsing network part '%s'", s2);
+		goto default_none;
+	}
+
+	memcpy (sa6, result->ai_addr, result->ai_addrlen);
+	freeaddrinfo (result);
+
+#ifdef INET_NETWORK_DEBUG
+	char sdebug[INET6_ADDRSTRLEN];
+	pgm_sockaddr_ntop (sa6, sdebug, sizeof(sdebug));
+	pgm_debug ("IPv6 network address: %s", sdebug);
+#endif
+
+	p++;
+	while (*p)
+	{
+		if (isdigit(*p)) {
+			val = 10 * val + (*p - '0');
+		} else {
+			pgm_debug ("failed parsing subnet size due to character '%c'", *p);
+			goto default_none;
+		}
+		p++;
+	}
+	if (val == 0 || val > 128) {
+		pgm_debug ("subnet size invalid (%d)", val);
+		goto default_none;
+	}
+	pgm_debug ("subnet size %i", val);
+
+/* zero out host bits */
+	const unsigned suffix_length = 128 - val;
+	for (int i = suffix_length, j = 15; i > 0; i -= 8, --j)
+	{
+		sa6->sin6_addr.s6_addr[ j ] &= i >= 8 ? 0x00 : (unsigned)(( 0xffU << i ) & 0xffU );
+	}
+
+#ifdef INET_NETWORK_DEBUG
+	pgm_sockaddr_ntop (sa6, sdebug, sizeof(sdebug));
+	pgm_debug ("effective IPv6 network address after subnet mask: %s", sdebug);
+#endif
+	return 0;
+
+default_none:
+	memset (sa6, 0, sizeof(*sa6));
+	sa6->sin6_family = AF_INET6;
+	memset (&sa6->sin6_addr, 0xff, sizeof(struct in6_addr));	/* equivalent to IN6ADDR_NONE */
+	return -1;
 }
 
 /* eof */
