1a231104e2ba42545407b0132d032e738bccca53
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2010-12-13 07:57:40 +0000

    * Add internal API call pgm_inet_makeaddr to convert pre-CDIR network addresses to CIDR period style. * Convert result from native getnetbyname with pgm_inet_makeaddr. * Copy buffer from native getnetbyname wrapper.

--- openpgm/pgm/test/test.conf.pl
+++ openpgm/pgm/test/test.conf.pl
@@ -4,20 +4,20 @@ use vars qw ( %config );
 %config = (
 	app => {
 # Linux host
-#		host	=> 'ayaka',
-#		ip	=> '10.6.28.31',
-#		cmd	=> 'sudo /miru/projects/openpgm/pgm/ref/release-Linux-x86_64/test/app',
-#		network => 'eth0;239.192.0.1'
+		host	=> 'ayaka',
+		ip	=> '10.6.28.31',
+		cmd	=> 'sudo /miru/projects/openpgm/pgm/ref/release-Linux-x86_64/test/app',
+		network => 'eth0;239.192.0.1'
 # Solaris host
 #		host	=> 'ryoko',
 #		ip	=> '10.6.28.36',
 #		cmd	=> 'sudo LD_LIBRARY_PATH=/opt/glib-sunstudio/lib:$LD_LIBRARY_PATH /miru/projects/openpgm/pgm/ref/release-SunOS-sun4u-sunstudio/test/app',
 #		network => 'eri0;239.192.0.1'
 # Windows host
-		host	=> 'Administrator@sora',
-		ip	=> '10.6.28.35',
-		cmd	=> '/cygdrive/c/temp/app.exe',
-		network	=> '10.6.28.35;239.192.0.1'
+#		host	=> 'Administrator@sora',
+#		ip	=> '10.6.28.35',
+#		cmd	=> '/cygdrive/c/temp/app.exe',
+#		network	=> '10.6.28.35;239.192.0.1'
 	},
 	mon => {
 		host	=> 'momo',
