c4ff9c97b7b781eedad4cdb569d73df30d7b48c3
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2011-02-07 03:48:45 +0000

    * Add atomic ops as dependency to ticket spinlocks. * Add psuedo-code notes to all atomic ops. * Replace __asm with __asm__. * Add inline assembler for atomic inc and dec. * Force MSVC intrinsics for remaining ops. * Flatten lock prefix in inline assembler. * Add 16-bit word atomic load & store API to note requirements on writer unlock. * Cleanup ticket lock code with internal atomic op API. * Add 8-bit and 16-bit word atomic add, inc, xadd, xinc ops. * Update 32-bit CAS assembler for bool return instead of original destination value.

--- trunk/openpgm/pgm/include/impl/ticket.h
+++ trunk/openpgm/pgm/include/impl/ticket.h
@@ -41,6 +41,7 @@ typedef union pgm_rwticket_t pgm_rwticket_t;
 #	include <windows.h>
 #endif
 #include <pgm/types.h>
+#include <pgm/atomic.h>
 #include <impl/thread.h>
 
 PGM_BEGIN_DECLS
@@ -112,6 +113,242 @@ union pgm_rwticket_t {
 #	pragma pack()
 #endif
 
+/* additional required atomic ops */
+
+/* 32-bit word CAS, returns TRUE if swap occurred.
+ *
+ *	if (*atomic == oldval) {
+ *		*atomic = newval;
+ *		return TRUE;
+ *	}
+ *	return FALSE;
+ */
+
+static inline
+bool
+pgm_atomic_compare_and_exchange32 (
+	volatile uint32_t*	atomic,
+	const uint32_t		newval,
+	const uint32_t		oldval
+	)
+{
+#if defined( __GNUC__ ) && ( defined( __i386__ ) || defined( __x86_64__ ) )
+	uint8_t result;
+	__asm__ volatile ("lock; cmpxchgl %2, %0\n\t"
+			  "setz %1\n\t"
+			: "+m" (*atomic), "=r" (result)
+			: "ir" (newval),  "a" (oldval)
+			: "memory", "cc"  );
+	return (bool)result;
+#elif defined( __GNUC__ ) && ( __GNUC__ * 100 + __GNUC_MINOR__ >= 401 )
+	return __sync_bool_compare_and_swap (atomic, oldval, newval);
+#elif defined( _WIN32 )
+	return (oldval == _InterlockedCompareExchange ((volatile LONG*)atomic, newval, oldval));
+#endif
+}
+
+/* byte addition.
+ *
+ *	*atomic += val;
+ */
+
+static inline
+void
+pgm_atomic_add8 (
+	volatile uint8_t*	atomic,
+	const uint8_t		val
+	)
+{
+#if defined( __GNUC__ ) && ( defined( __i386__ ) || defined( __x86_64__ ) )
+	__asm__ volatile ("lock; addb %1, %0"
+			: "=m" (*atomic)
+			: "ir" (val), "m" (*atomic)
+			: "memory", "cc"  );
+#elif defined( __GNUC__ ) && ( __GNUC__ * 100 + __GNUC_MINOR__ >= 401 )
+/* interchangable with __sync_fetch_and_add () */
+	__sync_add_and_fetch (atomic, val);
+#elif defined( _WIN32 )
+/* there is no _InterlockedExchangeAdd8() */
+#endif
+}
+
+/* byte addition returning original atomic value.
+ *
+ *	uint8_t oldval = *atomic;
+ *	*atomic += val;
+ *	return oldval;
+ */
+
+static inline
+uint8_t
+pgm_atomic_fetch_and_add8 (
+	volatile uint8_t*	atomic,
+	const uint8_t		val
+	)
+{
+#if defined( __GNUC__ ) && ( defined( __i386__ ) || defined( __x86_64__ ) )
+	uint8_t result;
+	__asm__ volatile ("lock; xaddb %0, %1"
+			: "=r" (result), "=m" (*atomic)
+			: "0" (val), "m" (*atomic)
+			: "memory", "cc"  );
+	return result;
+#elif defined( __GNUC__ ) && ( __GNUC__ * 100 + __GNUC_MINOR__ >= 401 )
+	return __sync_fetch_and_add (atomic, val);
+#elif defined( _WIN32 )
+/* there is no _InterlockedExchangeAdd8() */
+#endif
+}
+
+/* byte increment.
+ *
+ *	*atomic++;
+ */
+
+static inline
+void
+pgm_atomic_inc8 (
+	volatile uint8_t*	atomic
+	)
+{
+#if defined( __GNUC__ ) && ( defined( __i386__ ) || defined( __x86_64__ ) )
+	__asm__ volatile ("lock; incb %0"
+			: "+m" (*atomic)
+			:
+			: "memory", "cc"  );
+#else
+/* there is no _InterlockedIncrement8() */
+	pgm_atomic_add8 (atomic, 1);
+#endif
+}
+
+/* byte increment returning original atomic value.
+ *
+ *	uint8_t tmp = *atomic++;
+ *	return tmp;
+ */
+
+static inline
+uint8_t
+pgm_atomic_fetch_and_inc8 (
+	volatile uint8_t*	atomic
+	)
+{
+/* there is no _InterlockedIncrement8() */
+	return pgm_atomic_fetch_and_add8 (atomic, 1);
+}
+
+/* 16-bit word addition.
+ */
+
+static inline
+void
+pgm_atomic_add16 (
+	volatile uint16_t*	atomic,
+	const uint16_t		val
+	)
+{
+#if defined( __GNUC__ ) && ( defined( __i386__ ) || defined( __x86_64__ ) )
+	__asm__ volatile ("lock; addw %1, %0"
+			: "=m" (*atomic)
+			: "ir" (val), "m" (*atomic)
+			: "memory", "cc"  );
+#elif defined( __GNUC__ ) && ( __GNUC__ * 100 + __GNUC_MINOR__ >= 401 )
+/* interchangable with __sync_fetch_and_add () */
+	__sync_add_and_fetch (atomic, val);
+#elif defined( _WIN32 )
+/* there is no _InterlockedExchangeAdd16() */
+#endif
+}
+
+/* 16-bit word addition returning original atomic value.
+ */
+
+static inline
+uint16_t
+pgm_atomic_fetch_and_add16 (
+	volatile uint16_t*	atomic,
+	const uint16_t		val
+	)
+{
+#if defined( __GNUC__ ) && ( defined( __i386__ ) || defined( __x86_64__ ) )
+	uint16_t result;
+	__asm__ volatile ("lock; xaddw %0, %1"
+			: "=r" (result), "=m" (*atomic)
+			: "0" (val), "m" (*atomic)
+			: "memory", "cc"  );
+	return result;
+#elif defined( __GNUC__ ) && ( __GNUC__ * 100 + __GNUC_MINOR__ >= 401 )
+	return __sync_fetch_and_add (atomic, val);
+#elif defined( _WIN32 )
+/* there is no _InterlockedExchangeAdd16() */
+#endif
+}
+
+/* 16-bit word increment.
+ */
+
+static inline
+void
+pgm_atomic_inc16 (
+	volatile uint16_t*	atomic
+	)
+{
+#if defined( __GNUC__ ) && ( defined( __i386__ ) || defined( __x86_64__ ) )
+	__asm__ volatile ("lock; incw %0"
+			: "+m" (*atomic)
+			:
+			: "memory", "cc"  );
+#elif defined( _WIN32 )
+	_InterlockedIncrement16 ((volatile SHORT*)atomic);
+#else
+	pgm_atomic_add16 (atomic, 1);
+#endif
+}
+
+/* 16-bit word increment returning original atomic value.
+ */
+
+static inline
+uint16_t
+pgm_atomic_fetch_and_inc16 (
+	volatile uint16_t*	atomic
+	)
+{
+#if defined( _WIN32 )
+	return _InterlockedIncrement16 ((volatile SHORT*)atomic);
+#else
+	return pgm_atomic_fetch_and_add16 (atomic, 1);
+#endif
+}
+
+/* 16-bit word load.
+ */
+
+static inline
+uint16_t
+pgm_atomic_read16 (
+	const volatile uint16_t* atomic
+	)
+{
+	return *atomic;
+}
+
+/* 16-bit word store.
+ */
+
+static inline
+void
+pgm_atomic_write16 (
+	volatile uint16_t*	atomic,
+	const uint16_t		val
+	)
+{
+	*atomic = val;
+}
+
+
+/* ticket spinlocks */
 
 static inline void pgm_ticket_init (pgm_ticket_t* ticket) {
 	ticket->pgm_tkt_data32 = 0;
@@ -127,52 +364,20 @@ static inline bool pgm_ticket_trylock (pgm_ticket_t* ticket) {
 	pgm_ticket_t exchange, comparand;
 	comparand.pgm_tkt_user = comparand.pgm_tkt_ticket = exchange.pgm_tkt_ticket = user;
 	exchange.pgm_tkt_user = user + 1;
-#ifdef _WIN32
-	const LONG previous = _InterlockedCompareExchange (&ticket->pgm_tkt_data32, exchange.pgm_tkt_data32, comparand.pgm_tkt_data32);
-#else   /* GCC atomics */
-	const uint32_t previous = __sync_val_compare_and_swap (&ticket->pgm_tkt_data32, comparand.pgm_tkt_data32, exchange.pgm_tkt_data32);
-#endif
-	return (comparand.pgm_tkt_data32 == previous);
+	return pgm_atomic_compare_and_exchange32 (&ticket->pgm_tkt_data32, exchange.pgm_tkt_data32, comparand.pgm_tkt_data32);
 }
-
-#ifdef _WIN32
-static inline char _InterlockedExchangeAdd8 (volatile char* const Addend, const char Value) {
-	char result;
-	__asm volatile (  "lock\n\t"
-			  "xaddb %0, %1"
-			: "=r" (result), "=m" (*Addend)
-			: "0" (Value), "m" (*Addend)
-			: "memory", "cc"  );
-	return result;
-}
-
-static inline short _InterlockedExchangeAdd16 (volatile short* const Addend, const short Value) {
-	short result;
-	__asm volatile (  "lock\n\t"
-			  "xaddw %0, %1"
-			: "=r" (result), "=m" (*Addend)
-			: "0" (Value), "m" (*Addend)
-			: "memory", "cc"  );
-	return result;
-}
-
-static inline char _InterlockedIncrement8 (volatile char* const Addend) {
-	return _InterlockedExchangeAdd8 (Addend, 1);
-}
-#endif
 
 static inline void pgm_ticket_lock (pgm_ticket_t* ticket) {
+	const uint16_t user = pgm_atomic_fetch_and_inc16 (&ticket->pgm_tkt_user);
 #ifdef _WIN32
-	const uint16_t user = _InterlockedIncrement16 (&ticket->pgm_tkt_user);
 	unsigned spins = 0;
 	while (ticket->pgm_tkt_ticket != user)
 		if (!pgm_smp_system || 0 == (++spins % 200))
 			SwitchToThread();
 		else
-			YieldProcessor();
+			YieldProcessor();			/* hyper-threading pause */
 #elif defined( __i386__ ) || defined( __i386 ) || defined( __x86_64__ ) || defined( __amd64 )
 /* GCC atomics */
-	const uint16_t user = __sync_fetch_and_add (&ticket->pgm_tkt_user, 1);
 	unsigned spins = 0;
 	while (ticket->pgm_tkt_ticket != user)
 		if (!pgm_smp_system || 0 == (++spins % 200))
@@ -180,21 +385,16 @@ static inline void pgm_ticket_lock (pgm_ticket_t* ticket) {
 		else
 			__asm volatile ("pause" ::: "memory");	/* hyper-threading pause */
 #else
-	const uint16_t user = __sync_fetch_and_add (&ticket->pgm_tkt_user, 1);
 	while (ticket->pgm_tkt_ticket != user)
 		sched_yield();
 #endif
 }
 
 static inline void pgm_ticket_unlock (pgm_ticket_t* ticket) {
-#ifdef _WIN32
-	_InterlockedIncrement16 (&ticket->pgm_tkt_ticket);
-#else
-/* interchangable with __sync_fetch_and_add () */
-	__sync_add_and_fetch (&ticket->pgm_tkt_ticket, 1);
-#endif
+	pgm_atomic_inc16 (&ticket->pgm_tkt_ticket);
 }
 
+/* read-write ticket spinlocks */
 
 static inline void pgm_rwticket_init (pgm_rwticket_t* rwticket) {
 	rwticket->pgm_rwtkt_data32 = 0;
@@ -206,62 +406,48 @@ static inline void pgm_rwticket_free (pgm_rwticket_t* rwticket) {
 }
 
 static inline void pgm_rwticket_reader_lock (pgm_rwticket_t* rwticket) {
+	const uint8_t user = pgm_atomic_fetch_and_inc8 (&rwticket->pgm_rwtkt_user);
 #ifdef _WIN32
-	const uint8_t user = _InterlockedIncrement8 (&rwticket->pgm_rwtkt_user);
 	unsigned spins = 0;
 	while (rwticket->pgm_rwtkt_read != user)
 		if (!pgm_smp_system || 0 == (++spins % 200))
 			SwitchToThread();
 		else
 			YieldProcessor();
-	_InterlockedIncrement8 (&rwticket->pgm_rwtkt_read);
 #elif defined( __i386__ ) || defined( __i386 ) || defined( __x86_64__ ) || defined( __amd64 )
-	const uint8_t user = __sync_fetch_and_add (&rwticket->pgm_rwtkt_user, 1);
 	unsigned spins = 0;
 	while (rwticket->pgm_rwtkt_read != user)
 		if (!pgm_smp_system || 0 == (++spins % 200))
 			sched_yield();
 		else
 			__asm volatile ("pause" ::: "memory");
-	__sync_add_and_fetch (&rwticket->pgm_rwtkt_read, 1);
 #else
-	const uint8_t user = __sync_fetch_and_add (&rwticket->pgm_rwtkt_user, 1);
 	while (rwticket->pgm_rwtkt_read != user)
 		sched_yield();
-	__sync_add_and_fetch (&rwticket->pgm_rwtkt_read, 1);
 #endif
+	pgm_atomic_inc8 (&rwticket->pgm_rwtkt_read);
 }
 
 static inline bool pgm_rwticket_reader_trylock (pgm_rwticket_t* rwticket) {
 	const uint8_t user = rwticket->pgm_rwtkt_user;
 	pgm_rwticket_t exchange, comparand;
-#ifdef _WIN32
-	LONG previous;
-#else
-	uint32_t previous;
-#endif
+	exchange.pgm_rwtkt_data32 = comparand.pgm_rwtkt_data32 = 0;
 	exchange.pgm_rwtkt_write = comparand.pgm_rwtkt_write = rwticket->pgm_rwtkt_write;
 	comparand.pgm_rwtkt_user = comparand.pgm_rwtkt_read = user;
 	exchange.pgm_rwtkt_user = exchange.pgm_rwtkt_read = user + 1;
-#ifdef _WIN32
-	previous = _InterlockedCompareExchange (&rwticket->pgm_rwtkt_data32, exchange.pgm_rwtkt_data32, comparand.pgm_rwtkt_data32);
-#else
-	previous = __sync_val_compare_and_swap (&rwticket->pgm_rwtkt_data32, comparand.pgm_rwtkt_data32, exchange.pgm_rwtkt_data32);
-#endif
-	return (comparand.pgm_rwtkt_data32 == previous);
+	return pgm_atomic_compare_and_exchange32 (&rwticket->pgm_rwtkt_data32, exchange.pgm_rwtkt_data32, comparand.pgm_rwtkt_data32);
 }
 
 static inline void pgm_rwticket_reader_unlock(pgm_rwticket_t* rwticket) {
-#ifdef _WIN32
-	_InterlockedIncrement8 (&rwticket->pgm_rwtkt_write);
-#else
-	__sync_add_and_fetch (&rwticket->pgm_rwtkt_write, 1);
-#endif
+	pgm_atomic_inc8 (&rwticket->pgm_rwtkt_write);
 }
 
+/* users++
+ */
+
 static inline void pgm_rwticket_writer_lock (pgm_rwticket_t* rwticket) {
+	const uint8_t user = pgm_atomic_fetch_and_inc8 (&rwticket->pgm_rwtkt_user);
 #ifdef _WIN32
-	const uint8_t user = _InterlockedIncrement8 (&rwticket->pgm_rwtkt_user);
 	unsigned spins = 0;
 	while (rwticket->pgm_rwtkt_write != user)
 		if (!pgm_smp_system || 0 == (++spins % 200))
@@ -269,7 +455,6 @@ static inline void pgm_rwticket_writer_lock (pgm_rwticket_t* rwticket) {
 		else
 			YieldProcessor();
 #elif defined( __i386__ ) || defined( __i386 ) || defined( __x86_64__ ) || defined( __amd64 )
-	const uint8_t user = __sync_fetch_and_add (&rwticket->pgm_rwtkt_user, 1);
 	unsigned spins = 0;
 	while (rwticket->pgm_rwtkt_write != user)
 		if (!pgm_smp_system || 0 == (++spins % 200))
@@ -277,39 +462,33 @@ static inline void pgm_rwticket_writer_lock (pgm_rwticket_t* rwticket) {
 		else
 			__asm volatile ("pause" ::: "memory");
 #else
-	const uint8_t user = __sync_fetch_and_add (&rwticket->pgm_rwtkt_user, 1);
 	while (rwticket->pgm_rwtkt_write != user)
 		sched_yield();
 #endif
 }
 
+/* users++
+ */
+
 static inline bool pgm_rwticket_writer_trylock (pgm_rwticket_t* rwticket) {
 	const uint8_t user = rwticket->pgm_rwtkt_user;
 	pgm_rwticket_t exchange, comparand;
-#ifdef _WIN32
-	LONG previous;
-#else
-	uint32_t previous;
-#endif
+	exchange.pgm_rwtkt_data32 = comparand.pgm_rwtkt_data32 = 0;
 	exchange.pgm_rwtkt_read = comparand.pgm_rwtkt_read = rwticket->pgm_rwtkt_read;
 	exchange.pgm_rwtkt_write = comparand.pgm_rwtkt_user = comparand.pgm_rwtkt_write = user;
 	exchange.pgm_rwtkt_user = user + 1;
-#ifdef _WIN32
-	previous = _InterlockedCompareExchange (&rwticket->pgm_rwtkt_data32, exchange.pgm_rwtkt_data32, comparand.pgm_rwtkt_data32);
-#else
-	previous = __sync_val_compare_and_swap (&rwticket->pgm_rwtkt_data32, comparand.pgm_rwtkt_data32, exchange.pgm_rwtkt_data32);
-#endif
-	return (comparand.pgm_rwtkt_data32 == previous);
+	return pgm_atomic_compare_and_exchange32 (&rwticket->pgm_rwtkt_data32, exchange.pgm_rwtkt_data32, comparand.pgm_rwtkt_data32);
 }
 
-/* requires 16-bit atomic read and write */
+/* read-ticket++, write-ticket++;
+ */
 
 static inline void pgm_rwticket_writer_unlock (pgm_rwticket_t* rwticket) {
 	pgm_rwticket_t t;
-	t.pgm_rwtkt_data16 = rwticket->pgm_rwtkt_data16;
+	t.pgm_rwtkt_data16 = pgm_atomic_read16 (&rwticket->pgm_rwtkt_data16);
 	t.pgm_rwtkt_write++;
 	t.pgm_rwtkt_read++;
-	rwticket->pgm_rwtkt_data16 = t.pgm_rwtkt_data16;
+	pgm_atomic_write16 (&rwticket->pgm_rwtkt_data16, t.pgm_rwtkt_data16);
 }
 
 PGM_END_DECLS
