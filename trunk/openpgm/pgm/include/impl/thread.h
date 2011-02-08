365eed7b512c50b7fe6aed684f7e75551f8c9a7f
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2011-02-08 23:08:49 +0000

    Bump version.

--- trunk/openpgm/pgm/include/impl/thread.h
+++ trunk/openpgm/pgm/include/impl/thread.h
@@ -2,6 +2,8 @@
  * 
  * mutexes and locks.
  *
+ * Copyright (c) 2010-2011 Miru Limited.
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
