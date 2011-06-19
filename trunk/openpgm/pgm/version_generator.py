6ce2aa6cc0e8129448d3210094bae66fbf730e77
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2011-06-19 17:12:06 +0000

    Minor build fixes.

--- trunk/openpgm/pgm/version_generator.py
+++ trunk/openpgm/pgm/version_generator.py
@@ -30,7 +30,7 @@ print """
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#Include <libpgmconfig.h>
+#include <libpgmconfig.h>
 #include <impl/framework.h>
 #include <pgm/version.h>
 
