05450d5fb4b55f19712a97d16121d10f2daaf5db
fnjordy <fnjordy@73fd2e2c-ad7e-11de-87bf-5f37c11c8191>
2011-06-27 15:06:44 +0000

    Update generators for Autoconf config.h wrapper.

--- trunk/openpgm/pgm/version_generator.py
+++ trunk/openpgm/pgm/version_generator.py
@@ -30,7 +30,9 @@ print """
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <config.h>
+#ifdef HAVE_CONFIG_H
+#	include <config.h>
+#endif
 #include <impl/framework.h>
 #include <pgm/version.h>
 
