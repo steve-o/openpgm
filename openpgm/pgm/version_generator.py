steve-o/openpgm

commit f4debdce3de8da1bd401d8542c5acbfaa0982b76

allow to override build date
to enable reproducible builds.
See https://reproducible-builds.org/ for why this is good
and https://reproducible-builds.org/specs/source-date-epoch/
for the definition of this variable.

openpgm/pgm/version_generator.py
@@ -4,8 +4,9 @@ import os
 import platform
 import time
 
-build_date = time.strftime ("%Y-%m-%d")
-build_time = time.strftime ("%H:%M:%S")
+timestamp = time.gmtime(int(os.environ.get('SOURCE_DATE_EPOCH', time.time()))
+build_date = time.strftime ("%Y-%m-%d", timestamp)
+build_time = time.strftime ("%H:%M:%S", timestamp)
 build_rev = ''.join (list (filter (str.isdigit, "$Revision$")))
 
 print ("""

commit 042960ef9fd3395cd411922d77c96c65f977040c

fix syntax error in version_generator.py
introduced in f4debdce3de8da1bd401d8542c5acbfaa0982b76

openpgm/pgm/version_generator.py
@@ -4,7 +4,7 @@ import os
 import platform
 import time
 
-timestamp = time.gmtime(int(os.environ.get('SOURCE_DATE_EPOCH', time.time()))
+timestamp = time.gmtime(int(os.environ.get('SOURCE_DATE_EPOCH', time.time())))
 build_date = time.strftime ("%Y-%m-%d", timestamp)
 build_time = time.strftime ("%H:%M:%S", timestamp)
 build_rev = ''.join (list (filter (str.isdigit, "$Revision$")))